b2883a494ea648793a27a922c568e5c472ada68c
KZ <zengke940420@gmail.com>
2022-12-21 01:02:08 -0500

    Counts tables and attempted parsing

---
+++ Scripts/ParsingCountsTables.R
@@ -0,0 +1,34 @@
+rm(list = ls())
+library(stringr)
+# makeMeta<-read.csv("Tessa_duke_newPipeline_count_table[1].csv", sep = ",", header = T, row.names = 1)
+# rownames(makeMeta)<-sapply(str_split(rownames(makeMeta), "_",  n = 4), `[`, 3)
+
+makeAMR<-read.delim("CountsTables/AMR_counts.tsv", sep = "\t", header = T, row.names = 1)
+# sapply(str_split(colnames(makeAMR), "_",  n = 4), `[`, 1)
+# length(unique(sapply(str_split(colnames(makeAMR), "_",  n = 4), `[`, 1)))
+colnames(makeAMR)<-sapply(str_split(colnames(makeAMR), "_", n = 4 ), `[`, 1)
+
+sampleNames<-unique(sapply(str_split(sapply(str_split(colnames(makeAMR), ".D", n = 2 ), `[`, 1), regex(".PRE", ignore_case = T), n = 2 ), `[`, 1))
+for (i in 1:length(sampleNames)) {
+  print(colnames(makeAMR)[grepl(sampleNames[i], colnames(makeAMR))])
+}
+
+allNames<-colnames(makeAMR)[grepl(sampleNames[1], colnames(makeAMR))]
+days<-sort(as.numeric(sapply(str_split(colnames(makeAMR)[grepl(sampleNames[1], colnames(makeAMR))], ".D", n = 2), `[`, 2)))
+# allDays<-sapply(str_split(colnames(makeAMR)[grepl(sampleNames[1], colnames(makeAMR))], ".D", n = 2), `[`, 2)
+orderedNames<-paste0(sampleNames[1], days, sep = "D")
+if (sum(grepl("PRE", allNames, ignore.case = T))>0)
+  days<-append(paste0(sampleNames[1], "PRE"), days)
+
+
+
+makeRGI<-read.delim("CountsTables/RGI_counts.tsv", sep = "\t", header = T, row.names = 1)
+# sapply(str_split(colnames(makeRGI), "_",  n = 4), `[`, 1)
+# length(unique(sapply(str_split(colnames(makeRGI), "_",  n = 4), `[`, 1)))
+colnames(makeRGI)<-sapply(str_split(colnames(makeRGI), "_", n = 4 ), `[`, 1)
+
+makeVSEARCH<-read.delim("CountsTables/vsearch_counts.tsv", sep = "\t", header = T, row.names = 1)
+# sapply(str_split(colnames(makeRGI), "_",  n = 4), `[`, 1)
+# length(unique(sapply(str_split(colnames(makeRGI), "_",  n = 4), `[`, 1)))
+colnames(makeVSEARCH)<-sapply(str_split(colnames(makeVSEARCH), "_", n = 4 ), `[`, 1)
+
