6e5ad7bb57a0d2e5f6d246fb8f39f887f4afb679
KZ <zengke940420@gmail.com>
2023-02-21 03:29:29 -0500

    log10(MDS+10)

--- Scripts/TimeVsMDS1234.R
+++ Scripts/TimeVsMDS1234.R
@@ -76,24 +76,25 @@ pdf("Plots/TimeVsMDS1234(Bracken).pdf", width=12, height=12)
 par(mfrow=c(2,2))
 par(mar=c(5,6,4,1)+.1)
 MDS<-summary(capscale(t(brackenT)~1,distance = "bray"))[["sites"]][,c(1,2,3,4)]
+MDS<-log10(MDS+10)
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,1], 
      col = "tan2", pch = 19, 
-     xlab = "Days", ylab = "MDS1",
+     xlab = "Days", ylab = "log10(MDS1+10)",
      main = "Bracken MDS1")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,2], 
      col = "tan2", pch = 19, 
-     xlab = "Days", ylab = "MDS2",
+     xlab = "Days", ylab = "log10(MDS2+10)",
      main = "Bracken MDS2")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,3], 
      col = "tan2", pch = 19, 
-     xlab = "Days", ylab = "MDS3",
+     xlab = "Days", ylab = "log10(MDS3+10)",
      main = "Bracken MDS3")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,4], 
      col = "tan2", pch = 19, 
-     xlab = "Days", ylab = "MDS4",
+     xlab = "Days", ylab = "log10(MDS4+10)",
      main = "Bracken MDS4")
 dev.off()
 
@@ -102,24 +103,25 @@ pdf("Plots/TimeVsMDS1234(AMR).pdf", width=12, height=12)
 par(mfrow=c(2,2))
 par(mar=c(5,6,4,1)+.1)
 MDS<-summary(capscale(t(amrT)~1,distance = "bray"))[["sites"]][,c(1,2,3,4)]
+MDS<-log10(MDS+10)
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,1], 
      col = "coral3", pch = 19, 
-     xlab = "Days", ylab = "MDS1",
+     xlab = "Days", ylab = "log10(MDS1+10)",
      main = "AMR MDS1")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,2], 
      col = "coral3", pch = 19, 
-     xlab = "Days", ylab = "MDS2",
+     xlab = "Days", ylab = "log10(MDS2+10)",
      main = "AMR MDS2")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,3], 
      col = "coral3", pch = 19, 
-     xlab = "Days", ylab = "MDS3",
+     xlab = "Days", ylab = "log10(MDS3+10)",
      main = "AMR MDS3")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,4], 
      col = "coral3", pch = 19, 
-     xlab = "Days", ylab = "MDS4",
+     xlab = "Days", ylab = "log10(MDS4+10)",
      main = "AMR MDS4")
 dev.off()
 
@@ -128,24 +130,25 @@ pdf("Plots/TimeVsMDS1234(RGI).pdf", width=12, height=12)
 par(mfrow=c(2,2))
 par(mar=c(5,6,4,1)+.1)
 MDS<-summary(capscale(t(rgiT)~1,distance = "bray"))[["sites"]][,c(1,2,3,4)]
+MDS<-log10(MDS+10)
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,1], 
      col = "cornflowerblue", pch = 19, 
-     xlab = "Days", ylab = "MDS1",
+     xlab = "Days", ylab = "log10(MDS1+10)",
      main = "RGI MDS1")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,2], 
      col = "cornflowerblue", pch = 19, 
-     xlab = "Days", ylab = "MDS2",
+     xlab = "Days", ylab = "log10(MDS2+10)",
      main = "RGI MDS2")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,3], 
      col = "cornflowerblue", pch = 19, 
-     xlab = "Days", ylab = "MDS3",
+     xlab = "Days", ylab = "log10(MDS3+10)",
      main = "RGI MDS3")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,4], 
      col = "cornflowerblue", pch = 19, 
-     xlab = "Days", ylab = "MDS4",
+     xlab = "Days", ylab = "log10(MDS4+10)",
      main = "RGI MDS4")
 dev.off()
 
@@ -154,24 +157,25 @@ pdf("Plots/TimeVsMDS1234(vsearch).pdf", width=12, height=12)
 par(mfrow=c(2,2))
 par(mar=c(5,6,4,1)+.1)
 MDS<-summary(capscale(t(vsearchT)~1,distance = "bray"))[["sites"]][,c(1,2,3,4)]
+MDS<-log10(MDS+10)
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,1], 
      col = "olivedrab4", pch = 19, 
-     xlab = "Days", ylab = "MDS1",
+     xlab = "Days", ylab = "log10(MDS1+10)",
      main = "vsearch MDS1")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,2], 
      col = "olivedrab4", pch = 19, 
-     xlab = "Days", ylab = "MDS2",
+     xlab = "Days", ylab = "log10(MDS2+10)",
      main = "vsearch MDS2")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,3], 
      col = "olivedrab4", pch = 19, 
-     xlab = "Days", ylab = "MDS3",
+     xlab = "Days", ylab = "log10(MDS3+10)",
      main = "vsearch MDS3")
 
 plot(dukeSamples[rownames(MDS), 2], MDS[ ,4], 
      col = "olivedrab4", pch = 19, 
-     xlab = "Days", ylab = "MDS4",
+     xlab = "Days", ylab = "log10(MDS4+10)",
      main = "vsearch MDS4")
 dev.off()
 
