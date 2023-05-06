e7f780093937d6f7818bc893033e0f0575b5cd1b
KZ <zengke940420@gmail.com>
2023-05-05 23:14:16 -0400

    sorted by F-test pvalue, plots of second order mixed linear model with samples in less than 600 days.

---
+++ Scripts/SortedByFSecondOrderMixedLmLessThan600DaysPlots.R
@@ -0,0 +1,275 @@
+rm(list = ls())
+library(stringr)
+library("RColorBrewer")
+library("vegan")
+library("dplyr")
+library("nlme")
+#metadata
+dukeSamples<-read.csv("Duke_samples_meta.csv", header = T, sep = ",")
+rownames(dukeSamples)<-dukeSamples$Seq_sample
+rownames(dukeSamples)<-sapply(str_split(rownames(dukeSamples), "_", n = 2), `[`, 2)
+dukeSamples$ID<-paste0("D", gsub("pre","" , sapply(str_split(dukeSamples$sample, "D", n = 3), `[`, 2), ignore.case = T)) #adding sample ID
+dukeSamples$ID<-gsub("npe", "", dukeSamples$ID, ignore.case = T)
+dukeSamples<-dukeSamples %>% mutate(bins = case_when(between(dukeSamples$Timepoint, -100, -2) ~ "PRE",
+                                                     between(dukeSamples$Timepoint, -3, 10) ~ "D7",
+                                                     between(dukeSamples$Timepoint, 11, 17) ~ "D14",
+                                                     between(dukeSamples$Timepoint, 18, 24) ~ "D21",
+                                                     between(dukeSamples$Timepoint, 25, 45) ~ "D35",
+                                                     between(dukeSamples$Timepoint, 46, 75) ~ "D60",
+                                                     between(dukeSamples$Timepoint, 76, 965) ~ "D100"))
+
+#bracken
+brackenT<-read.delim("CountsTables/duke_bracken.csv", sep = ",", header = T, row.names = 1)
+brackenT<-brackenT[, -c(1,2)] #get rid of taxonomy ID and level
+brackenT<-brackenT[, grepl("num", colnames(brackenT))] #get rid of fractions
+
+colnames(brackenT)<-gsub(".bracken.out_num", "", colnames(brackenT)) #get rid of useless info
+SampleWithDots<-sapply(str_count(colnames(brackenT), "\\."), `[`, 1) == 2
+colnames(brackenT)[SampleWithDots]<-sub("\\.", "", colnames(brackenT)[SampleWithDots]) #get rid of random "."s in sample names
+colnames(brackenT)<-sub("\\.", "-", colnames(brackenT)) #replace . with - to match with metadata
+colnames(brackenT)<-sapply(str_split(colnames(brackenT), "_", n = 2), `[`, 2) #keeping sequencing info for matching
+
+brackenT<-brackenT[, -which(colnames(brackenT) %in% c(rownames(dukeSamples)[dukeSamples$Timepoint>600])) ]
+
+#AMR
+amrT<-read.delim("CountsTables/AMR_counts.tsv", sep = "\t", header = T, row.names = 2)
+amrT<-amrT[,-1]
+amrT<-amrT[, grepl("^D", colnames(amrT))]
+colnames(amrT)<-gsub(".amrfinder.txt", "", colnames(amrT)) #get rid of useless info
+SampleWithDots<-sapply(str_count(colnames(amrT), "\\."), `[`, 1) == 2
+colnames(amrT)[SampleWithDots]<-sub("\\.", "", colnames(amrT)[SampleWithDots]) #get rid of random "."s in sample names
+colnames(amrT)<-sub("\\.", "-", colnames(amrT)) #replace . with - to match with metadata
+colnames(amrT)<-sapply(str_split(colnames(amrT), "_", n = 2), `[`, 2)
+amrT<-amrT[, -27]
+
+amrT<-amrT[, -which(colnames(amrT) %in% c(rownames(dukeSamples)[dukeSamples$Timepoint>600])) ]
+
+#RGI
+rgiT<-read.delim("CountsTables/RGI_counts.tsv", sep = "\t", header = T, row.names = 2)
+rgiT<-rgiT[, -1]
+rgiT<-rgiT[, grepl("^D", colnames(rgiT))]
+colnames(rgiT)<-gsub(".rgi.txt", "", colnames(rgiT)) #get rid of useless info
+SampleWithDots<-sapply(str_count(colnames(rgiT), "\\."), `[`, 1) == 2
+colnames(rgiT)[SampleWithDots]<-sub("\\.", "", colnames(rgiT)[SampleWithDots]) #get rid of random "."s in sample names
+colnames(rgiT)<-sub("\\.", "-", colnames(rgiT)) #replace . with - to match with metadata
+colnames(rgiT)<-sapply(str_split(colnames(rgiT), "_", n = 2), `[`, 2)
+
+rgiT<-rgiT[, -which(colnames(rgiT) %in% c(rownames(dukeSamples)[dukeSamples$Timepoint>600])) ]
+
+#vsearch
+vsearchT<-read.delim("CountsTables/vsearch_counts.tsv", sep = "\t", header = T, row.names = 2)
+vsearchT<-vsearchT[, -1] #get rid of index column
+vsearchT<-vsearchT[, grepl("^D", colnames(vsearchT))]
+colnames(vsearchT)<-gsub(".txt", "", colnames(vsearchT)) #get rid of useless info
+
+SampleWithDots<-sapply(str_count(colnames(vsearchT), "\\."), `[`, 1) == 2
+colnames(vsearchT)[SampleWithDots]<-sub("\\.", "", colnames(vsearchT)[SampleWithDots]) #get rid of random "."s in sample names
+colnames(vsearchT)<-sub("\\.", "-", colnames(vsearchT)) #replace . with - to match with metadata
+colnames(vsearchT)<-sapply(str_split(colnames(vsearchT), "_", n = 2), `[`, 2)
+
+vsearchT<-vsearchT[, -which(colnames(vsearchT) %in% c(rownames(dukeSamples)[dukeSamples$Timepoint>600])) ]
+
+#Normalization
+n<-colSums(brackenT)
+sumx<-sum(brackenT)
+for (i in 1:ncol(brackenT)) {
+  brackenT[,i]<-brackenT[,i]/n[i]
+}
+brackenT<-log10(brackenT*(sumx/ncol(brackenT))+1)
+
+n<-colSums(amrT)
+sumx<-sum(amrT)
+for (i in 1:ncol(amrT)) {
+  amrT[,i]<-amrT[,i]/n[i]
+}
+amrT<-log10(amrT*(sumx/ncol(amrT))+1)
+
+n<-colSums(rgiT)
+sumx<-sum(rgiT)
+for (i in 1:ncol(rgiT)) {
+  rgiT[,i]<-rgiT[,i]/n[i]
+}
+rgiT<-log10(rgiT*(sumx/ncol(rgiT))+1)
+
+n<-colSums(vsearchT)
+sumx<-sum(vsearchT)
+for (i in 1:ncol(vsearchT)) {
+  vsearchT[,i]<-vsearchT[,i]/n[i]
+}
+vsearchT<-log10(vsearchT*(sumx/ncol(vsearchT))+1)
+
+#meta for each table
+metaBracken<-dukeSamples[colnames(brackenT), ]
+metaAMR<-dukeSamples[colnames(amrT), ]
+metaRGI<-dukeSamples[colnames(rgiT), ]
+metaVsearch<-dukeSamples[colnames(vsearchT), ]
+
+#lm 2nd order
+pdf("Plots/SortedByFSecondOrderMixedLm(BrackenLessThan600Days).pdf", width=12, height=18)
+par(mfrow=c(3, 2))
+par(mar=c(5, 6, 4, 1)+.1)
+brackenT<-brackenT[apply(brackenT == 0, 1, sum) <= (ncol(brackenT)*0.8), ]
+pvals<-vector()
+Fpval<-vector()
+for (i in 1:nrow(brackenT)) {
+  myM<-data.frame(unlist(brackenT[i, ]), metaBracken$Timepoint, metaBracken$ID)
+  colnames(myM)<-c("counts", "timePoint", "ID")
+  lineM<-lme(counts ~ timePoint, random = ~1 | ID, data = myM)
+  polyM<-lme(counts ~ poly(timePoint, 2), random = ~1 | ID, data = myM)
+  
+  reducedError<-sum(resid(lineM)^2)
+  fullError<-sum(resid(polyM)^2)
+  
+  reducedDF<-nrow(myM)-2
+  fullDF<-nrow(myM)-3
+  
+  myF <- ((reducedError - fullError)/(reducedDF - fullDF))/(fullError/fullDF)
+  
+  Fpval[i]<-pf(myF, 1, fullDF, lower.tail = F)
+  pvals[i]<-anova(polyM)[2,4]
+}
+adjPvals<-p.adjust(pvals, method = "BH")
+adjFpvals<-p.adjust(Fpval, method = "BH")
+
+plot_order<-order(Fpval)
+for (i in 1:nrow(brackenT)) {
+  mainText<-paste0(rownames(brackenT)[plot_order[i]], 
+                   "\nANOVA P=", signif(pvals[plot_order[i]], 3), ", ", "adj. ANOVA-P=", signif(adjPvals[plot_order[i]], 3), 
+                   "\nLine vs. Poly F-P=", signif(Fpval[plot_order[i]], 3), ", ", "adj. F-P=", signif(adjFpvals[plot_order[i]], 3))
+  myM<-data.frame(unlist(brackenT[plot_order[i], ]), metaBracken$Timepoint, metaBracken$ID)
+  colnames(myM)<-c("counts", "timePoint", "ID")
+  Model<-lme(counts ~ poly(timePoint, 2), random = ~1 | ID, data = myM)
+  plot(metaBracken$Timepoint, unlist(brackenT[plot_order[i],]), 
+       pch = 19, col = "tan2", 
+       main = mainText,
+       xlab = "TimePoints", ylab = "Counts(Log10)")
+  lines(sort(metaBracken$Timepoint), fitted(Model)[order(metaBracken$Timepoint)], 
+        col = "grey", type = "l")
+}
+dev.off()
+
+pdf("Plots/SortedByFSecondOrderMixedLm(AMRLessThan600Days).pdf", width=12, height=18)
+par(mfrow=c(3, 2))
+par(mar=c(5, 6, 4, 1)+.1)
+amrT<-amrT[apply(amrT == 0, 1, sum) <= (ncol(amrT)*0.8), ]
+pvals<-vector()
+Fpval<-vector()
+for (i in 1:nrow(amrT)) {
+  myM<-data.frame(unlist(amrT[i, ]), metaAMR$Timepoint, metaAMR$ID)
+  colnames(myM)<-c("counts", "timePoint", "ID")
+  lineM<-lme(counts ~ timePoint, random = ~1 | ID, data = myM)
+  polyM<-lme(counts ~ poly(timePoint, 2), random = ~1 | ID, data = myM)
+  
+  reducedError<-sum(resid(lineM)^2)
+  fullError<-sum(resid(polyM)^2)
+  
+  reducedDF<-nrow(myM)-2
+  fullDF<-nrow(myM)-3
+  
+  myF <- ((reducedError - fullError)/(reducedDF - fullDF))/(fullError/fullDF)
+  
+  Fpval[i]<-pf(myF, 1, fullDF, lower.tail = F)
+  pvals[i]<-anova(polyM)[2,4]
+}
+adjPvals<-p.adjust(pvals, method = "BH")
+adjFpvals<-p.adjust(Fpval, method = "BH")
+
+plot_order<-order(Fpval)
+for (i in 1:nrow(amrT)) {
+  mainText<-paste0(rownames(amrT)[plot_order[i]], "\nANOVA P=", signif(pvals[plot_order[i]], 3), ", ", "adj. ANOVA-P=", signif(adjPvals[plot_order[i]], 3), 
+                   "\nLine vs. Poly F-P=", signif(Fpval[plot_order[i]], 3), ", ", "adj. F-P=", signif(adjFpvals[plot_order[i]], 3))
+  myM<-data.frame(unlist(amrT[plot_order[i], ]), metaAMR$Timepoint, metaAMR$ID)
+  colnames(myM)<-c("counts", "timePoint", "ID")
+  Model<-lme(counts ~ poly(timePoint, 2), random = ~1 | ID, data = myM)
+  plot(metaAMR$Timepoint, unlist(amrT[plot_order[i],]), 
+       pch = 19, col = "coral3", 
+       main = mainText,
+       xlab = "TimePoints", ylab = "Counts(Log10)")
+  lines(sort(metaAMR$Timepoint), fitted(Model)[order(metaAMR$Timepoint)], 
+        col = "grey", type = "l")
+}
+dev.off()
+
+pdf("Plots/SortedByFSecondOrderMixedLm(RGILessThan600Days).pdf", width=12, height=18)
+par(mfrow=c(3, 2))
+par(mar=c(5, 6, 4, 1)+.1)
+rgiT<-rgiT[apply(rgiT == 0, 1, sum) <= (ncol(rgiT)*0.8), ]
+pvals<-vector()
+Fpval<-vector()
+for (i in 1:nrow(rgiT)) {
+  myM<-data.frame(unlist(rgiT[i, ]), metaRGI$Timepoint, metaRGI$ID)
+  colnames(myM)<-c("counts", "timePoint", "ID")
+  lineM<-lme(counts ~ timePoint, random = ~1 | ID, data = myM)
+  polyM<-lme(counts ~ poly(timePoint, 2), random = ~1 | ID, data = myM)
+  
+  reducedError<-sum(resid(lineM)^2)
+  fullError<-sum(resid(polyM)^2)
+  
+  reducedDF<-nrow(myM)-2
+  fullDF<-nrow(myM)-3
+  
+  myF <- ((reducedError - fullError)/(reducedDF - fullDF))/(fullError/fullDF)
+  
+  Fpval[i]<-pf(myF, 1, fullDF, lower.tail = F)
+  pvals[i]<-anova(polyM)[2,4]
+}
+adjPvals<-p.adjust(pvals, method = "BH")
+adjFpvals<-p.adjust(Fpval, method = "BH")
+
+plot_order<-order(Fpval)
+for (i in 1:nrow(rgiT)) {
+  mainText<-paste0(rownames(rgiT)[plot_order[i]], "\nANOVA P=", signif(pvals[plot_order[i]], 3), ", ", "adj. ANOVA-P=", signif(adjPvals[plot_order[i]], 3), 
+                   "\nLine vs. Poly F-P=", signif(Fpval[plot_order[i]], 3), ", ", "adj. F-P=", signif(adjFpvals[plot_order[i]], 3))
+  myM<-data.frame(unlist(rgiT[plot_order[i], ]), metaRGI$Timepoint, metaRGI$ID)
+  colnames(myM)<-c("counts", "timePoint", "ID")
+  Model<-lme(counts ~ poly(timePoint, 2), random = ~1 | ID, data = myM)
+  plot(metaRGI$Timepoint, unlist(rgiT[plot_order[i],]), 
+       pch = 19, col = "cornflowerblue", 
+       main = mainText,
+       xlab = "TimePoints", ylab = "Counts(Log10)")
+  lines(sort(metaRGI$Timepoint), fitted(Model)[order(metaRGI$Timepoint)], 
+        col = "grey", type = "l")
+}
+dev.off()
+
+pdf("Plots/SortedByFSecondOrderMixedLm(vsearchLessThan600Days).pdf", width=12, height=18)
+par(mfrow=c(3, 2))
+par(mar=c(5, 6, 4, 1)+.1)
+vsearchT<-vsearchT[apply(vsearchT == 0, 1, sum) <= (ncol(vsearchT)*0.8), ]
+pvals<-vector()
+Fpval<-vector()
+for (i in 1:nrow(vsearchT)) {
+  myM<-data.frame(unlist(vsearchT[i, ]), metaVsearch$Timepoint, metaVsearch$ID)
+  colnames(myM)<-c("counts", "timePoint", "ID")
+  lineM<-lme(counts ~ timePoint, random = ~1 | ID, data = myM)
+  polyM<-lme(counts ~ poly(timePoint, 2), random = ~1 | ID, data = myM)
+  
+  reducedError<-sum(resid(lineM)^2)
+  fullError<-sum(resid(polyM)^2)
+  
+  reducedDF<-nrow(myM)-2
+  fullDF<-nrow(myM)-3
+  
+  myF <- ((reducedError - fullError)/(reducedDF - fullDF))/(fullError/fullDF)
+  
+  Fpval[i]<-pf(myF, 1, fullDF, lower.tail = F)
+  pvals[i]<-anova(polyM)[2,4]
+}
+adjPvals<-p.adjust(pvals, method = "BH")
+adjFpvals<-p.adjust(Fpval, method = "BH")
+
+plot_order<-order(Fpval)
+for (i in 1:nrow(vsearchT)) {
+  mainText<-paste0(strsplit(rownames(vsearchT)[plot_order[i]], "\\|")[[1]][6],  "\nANOVA P=", signif(pvals[plot_order[i]], 3), ", ", "adj. ANOVA-P=", signif(adjPvals[plot_order[i]], 3), 
+                   "\nLine vs. Poly F-P=", signif(Fpval[plot_order[i]], 3), ", ", "adj. F-P=", signif(adjFpvals[plot_order[i]], 3))
+  myM<-data.frame(unlist(vsearchT[plot_order[i], ]), metaVsearch$Timepoint, metaVsearch$ID)
+  colnames(myM)<-c("counts", "timePoint", "ID")
+  Model<-lme(counts ~ poly(timePoint, 2), random = ~1 | ID, data = myM)
+  plot(metaVsearch$Timepoint, unlist(vsearchT[plot_order[i],]), 
+       pch = 19, col = "olivedrab4", 
+       main = mainText,
+       xlab = "TimePoints", ylab = "Counts(Log10)")
+  lines(sort(metaVsearch$Timepoint), fitted(Model)[order(metaVsearch$Timepoint)], 
+        col = "grey", type = "l")
+}
+dev.off()
