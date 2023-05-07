af38e1b961760a2d62469bfb529666cce1b311be
KZ <zengke940420@gmail.com>
2023-05-07 10:08:10 -0400

    pre processing the metadata and counts tables.

---
+++ Scripts/ProcessingCountsTablesAndMetadata.R
@@ -0,0 +1,73 @@
+#process metadata, bracken and three gene counts tables.
+rm(list = ls())
+library(stringr)
+library("dplyr")
+#metadata
+dukeSamples<-read.csv("Duke_samples_meta.csv", header = T, sep = ",")
+rownames(dukeSamples)<-dukeSamples$Seq_sample
+rownames(dukeSamples)<-sapply(str_split(rownames(dukeSamples), "_", n = 2), `[`, 2)
+dukeSamples$ID<-paste0("D", gsub("pre","" , sapply(str_split(dukeSamples$sample, "D", n = 3), `[`, 2), ignore.case = T)) #adding sample ID
+dukeSamples$ID<-gsub("npe", "", dukeSamples$ID, ignore.case = T)
+dukeSamples<-dukeSamples %>% mutate(bins = case_when(between(dukeSamples$Timepoint, -100, -2) ~ "PRE",
+                                                     between(dukeSamples$Timepoint, -3, 10) ~ "D7",
+                                                     between(dukeSamples$Timepoint, 11, 17) ~ "D14",
+                                                     between(dukeSamples$Timepoint, 18, 24) ~ "D21",
+                                                     between(dukeSamples$Timepoint, 25, 45) ~ "D35",
+                                                     between(dukeSamples$Timepoint, 46, 75) ~ "D60",
+                                                     between(dukeSamples$Timepoint, 76, 965) ~ "D100"))
+
+write.csv(dukeSamples, "metaWithBins.csv")
+
+#bracken
+brackenT<-read.delim("CountsTables/duke_bracken.csv", sep = ",", header = T, row.names = 1)
+brackenT<-brackenT[, -c(1,2)] #get rid of taxonomy ID and level
+brackenT<-brackenT[, grepl("num", colnames(brackenT))] #get rid of fractions
+
+colnames(brackenT)<-gsub(".bracken.out_num", "", colnames(brackenT)) #get rid of useless info
+SampleWithDots<-sapply(str_count(colnames(brackenT), "\\."), `[`, 1) == 2
+colnames(brackenT)[SampleWithDots]<-sub("\\.", "", colnames(brackenT)[SampleWithDots]) #get rid of random "."s in sample names
+colnames(brackenT)<-sub("\\.", "-", colnames(brackenT)) #replace . with - to match with metadata
+colnames(brackenT)<-sapply(str_split(colnames(brackenT), "_", n = 2), `[`, 2) #keeping sequencing info for matching
+
+write.csv(brackenT, "CountsTables/brackenProcessed.csv")
+
+#AMR
+amrT<-read.delim("CountsTables/AMR_counts.tsv", sep = "\t", header = T, row.names = 2)
+amrT<-amrT[,-1]
+amrT<-amrT[, grepl("^D", colnames(amrT))]
+colnames(amrT)<-gsub(".amrfinder.txt", "", colnames(amrT)) #get rid of useless info
+SampleWithDots<-sapply(str_count(colnames(amrT), "\\."), `[`, 1) == 2
+colnames(amrT)[SampleWithDots]<-sub("\\.", "", colnames(amrT)[SampleWithDots]) #get rid of random "."s in sample names
+colnames(amrT)<-sub("\\.", "-", colnames(amrT)) #replace . with - to match with metadata
+colnames(amrT)<-sapply(str_split(colnames(amrT), "_", n = 2), `[`, 2)
+#get rid of sample with all 0s
+amrT<-amrT[, -27]
+
+write.csv(amrT, "CountsTables/amrProcessed.csv")
+
+#RGI
+rgiT<-read.delim("CountsTables/RGI_counts.tsv", sep = "\t", header = T, row.names = 2)
+rgiT<-rgiT[, -1]
+rgiT<-rgiT[, grepl("^D", colnames(rgiT))]
+colnames(rgiT)<-gsub(".rgi.txt", "", colnames(rgiT)) #get rid of useless info
+SampleWithDots<-sapply(str_count(colnames(rgiT), "\\."), `[`, 1) == 2
+colnames(rgiT)[SampleWithDots]<-sub("\\.", "", colnames(rgiT)[SampleWithDots]) #get rid of random "."s in sample names
+colnames(rgiT)<-sub("\\.", "-", colnames(rgiT)) #replace . with - to match with metadata
+colnames(rgiT)<-sapply(str_split(colnames(rgiT), "_", n = 2), `[`, 2)
+
+write.csv(rgiT, "CountsTables/rgiProcessed.csv")
+
+#vsearch
+vsearchT<-read.delim("CountsTables/vsearch_counts.tsv", sep = "\t", header = T, row.names = 2)
+vsearchT<-vsearchT[, -1] #get rid of index column
+vsearchT<-vsearchT[, grepl("^D", colnames(vsearchT))]
+colnames(vsearchT)<-gsub(".txt", "", colnames(vsearchT)) #get rid of useless inåfo
+
+SampleWithDots<-sapply(str_count(colnames(vsearchT), "\\."), `[`, 1) == 2
+colnames(vsearchT)[SampleWithDots]<-sub("\\.", "", colnames(vsearchT)[SampleWithDots]) #get rid of random "."s in sample names
+colnames(vsearchT)<-sub("\\.", "-", colnames(vsearchT)) #replace . with - to match with metadata
+colnames(vsearchT)<-sapply(str_split(colnames(vsearchT), "_", n = 2), `[`, 2)
+
+rownames(vsearchT)<-sapply(str_split(rownames(vsearchT), "\\|", n = 6), `[`, 6)
+
+write.csv(vsearchT, "CountsTables/vsearchProcessed.csv")
