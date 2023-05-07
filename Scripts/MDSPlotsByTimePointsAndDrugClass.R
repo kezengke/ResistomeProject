3d723e2b921655ccfd33264310e08e56560786cd
KZ <zengke940420@gmail.com>
2023-05-07 15:47:52 -0400

    updated with processed counts table and metadata, added pvalue.

---
+++ Scripts/MDSPlotsByTimePointsAndDrugClass.R
@@ -0,0 +1,111 @@
+#MDS plots colored by timepoints based on MASTER_AMRlist_2023_03.csv Drug Type column
+rm(list = ls())
+library("vegan")
+library(stringr)
+library("dplyr")
+library("RColorBrewer")
+
+geneMatchT<-read.csv("MASTER_AMRlist_2023_03.csv", sep = ",", header = T)
+metaData<-read.csv("metaWithBins.csv", header = T, row.names = 1)
+
+#gene counts tables
+amrT<-read.csv("CountsTables/amrProcessed.csv", header = T, row.names = 1, check.names = F)
+rgiT<-read.csv("CountsTables/rgiProcessed.csv", header = T, row.names = 1, check.names = F)
+vsearchT<-read.csv("CountsTables/vsearchProcessed.csv", header = T, row.names = 1, check.names = F)
+
+#AMR
+#keep only the rows present in amr counts table in master list
+amrMatchT<-geneMatchT %>% filter(row_number() %in% match(rownames(amrT), geneMatchT$AMR.name))
+amrMatchT<-na.omit(amrMatchT)
+amrMatchT<-amrMatchT[, c(18,12)] #keep only the gene name and Drug Class columns
+
+drugClass<-unique(amrMatchT$Drug.Class)
+newamrT<-c()
+for (i in 1:length(drugClass)) {
+  genes<-amrMatchT %>% filter(amrMatchT$Drug.Class == Drug.Class[i])
+  currentFrame<-amrT[genes$AMR.name, ]
+  sumRow<-colSums(currentFrame)
+  newamrT<-rbind(newamrT, sumRow)
+}
+rownames(newamrT)<-drugClass
+#exclude samples with 0 counts in every row
+newamrT<-newamrT[, -which(colSums(newamrT) == 0)]
+
+#RGI
+#keep only the rows present in rgi counts table in master list
+rgiMatchT<-geneMatchT %>% filter(row_number() %in% match(rownames(rgiT), geneMatchT$RGI.CARD.Short.Name))
+rgiMatchT<-rgiMatchT[, c(15,12)] #keep only the gene name and Drug Type columns
+
+drugClass<-unique(rgiMatchT$Drug.Class)
+newrgiT<-c()
+for (i in 1:length(drugClass)) {
+  genes<-rgiMatchT %>% filter(rgiMatchT$Drug.Class == drugClass[i])
+  currentFrame<-rgiT[genes$RGI.CARD.Short.Name, ]
+  sumRow<-colSums(currentFrame)
+  newrgiT<-rbind(newrgiT, sumRow)
+}
+rownames(newrgiT)<-drugClass
+
+#VSEARCH
+#keep only the rows present in vsearch counts table in master list
+vsearchMatchT<-geneMatchT %>% filter(row_number() %in% match(rownames(vsearchT), geneMatchT$Vsearch.ARO.Name))
+vsearchMatchT<-vsearchMatchT[, c(7,12)] #keep only the gene name and Drug Type columns
+
+drugClass<-vsearchMatchT$Drug.Class
+newvsearchT<-c()
+for (i in 1:length(drugClass)) {
+  genes<-vsearchMatchT %>% filter(vsearchMatchT$Drug.Class == drugClass[i])
+  currentFrame<-vsearchT[genes$Vsearch.ARO.Name, ]
+  sumRow<-colSums(currentFrame)
+  newvsearchT<-rbind(newvsearchT, sumRow)
+}
+rownames(newvsearchT)<-drugClass
+
+metaAMR<-metaData[colnames(newamrT), , drop = F]
+metaRGI<-metaData[colnames(newrgiT), , drop = F]
+metaVSEARCH<-metaData[colnames(newvsearchT), , drop = F]
+
+pdf("Plots/MDSForDrugClass(ColoredByTimePoints).pdf", width=18, height=6)
+par(mfrow=c(1,3))
+par(mar=c(5,6,4,1)+.1)
+
+circleCol<-brewer.pal(length(unique(metaAMR$bins)), "Spectral")
+cols<-circleCol[factor(metaAMR$bins, levels = c("PRE", "D7", "D14", "D21", "D35", "D60", "D100"))]
+MDS<-capscale(t(newamrT)~1,distance = "bray")
+percentVariance<-MDS$CA$eig/sum(eigenvals(MDS))*100
+pval<-adonis2(t(newamrT)~metaAMR$bins, method="bray")$`Pr(>F)`[1]
+statusPlot<-ordiplot(MDS,choices=c(1,2),type="none",cex.lab=1,
+                     xlab=paste("MDS1  ", format(percentVariance[1], digits = 4), "%", sep = ""),
+                     ylab=paste("MDS2  ", format(percentVariance[2], digits = 4), "%", sep = ""),
+                     main=paste("AMR(DrugClass) n =", ncol(newamrT), "\nP-value:",pval))
+points(statusPlot,"sites", pch=19, cex=2.5, col=adjustcolor(cols, alpha.f = 0.5))
+ordiellipse(statusPlot, metaAMR$bins, kind="se", conf=0.95, lwd=4, draw = "lines", col=circleCol) 
+legend("topleft", c("PRE", "D7", "D14", "D21", "D35", "D60", "D100"), col = circleCol[1:11], cex = 1, pch = 16, bty = "n")
+
+circleCol<-brewer.pal(length(unique(metaRGI$bins)), "Spectral")
+cols<-circleCol[factor(metaRGI$bins, levels = c("PRE", "D7", "D14", "D21", "D35", "D60", "D100"))]
+MDS<-capscale(t(newrgiT)~1,distance = "bray")
+percentVariance<-MDS$CA$eig/sum(eigenvals(MDS))*100
+pval<-adonis2(t(newrgiT)~metaRGI$bins, method="bray")$`Pr(>F)`[1]
+statusPlot<-ordiplot(MDS,choices=c(1,2),type="none",cex.lab=1,
+                     xlab=paste("MDS1  ", format(percentVariance[1], digits = 4), "%", sep = ""),
+                     ylab=paste("MDS2  ", format(percentVariance[2], digits = 4), "%", sep = ""),
+                     main=paste("RGI(DrugClass) n =", ncol(newrgiT), "\nP-value:",pval))
+points(statusPlot,"sites", pch=19, cex=2.5, col=adjustcolor(cols, alpha.f = 0.5))
+ordiellipse(statusPlot, metaRGI$bins, kind="se", conf=0.95, lwd=4, draw = "lines", col=circleCol) 
+legend("topleft", c("PRE", "D7", "D14", "D21", "D35", "D60", "D100"), col = circleCol[1:11], cex = 1, pch = 16, bty = "n")
+
+circleCol<-brewer.pal(length(unique(metaVSEARCH$bins)), "Spectral")
+cols<-circleCol[factor(metaVSEARCH$bins, levels = c("PRE", "D7", "D14", "D21", "D35", "D60", "D100"))]
+MDS<-capscale(t(newvsearchT)~1,distance = "bray")
+percentVariance<-MDS$CA$eig/sum(eigenvals(MDS))*100
+pval<-adonis2(t(newvsearchT)~metaVSEARCH$bins, method="bray")$`Pr(>F)`[1]
+statusPlot<-ordiplot(MDS,choices=c(1,2),type="none",cex.lab=1,
+                     xlab=paste("MDS1  ", format(percentVariance[1], digits = 4), "%", sep = ""),
+                     ylab=paste("MDS2  ", format(percentVariance[2], digits = 4), "%", sep = ""),
+                     main=paste("vsearch(DrugClass) n =", ncol(newvsearchT), "\nP-value:",pval))
+points(statusPlot,"sites", pch=19, cex=2.5, col=adjustcolor(cols, alpha.f = 0.5))
+ordiellipse(statusPlot, metaVSEARCH$bins, kind="se", conf=0.95, lwd=4, draw = "lines", col=circleCol) 
+legend("topright", c("PRE", "D7", "D14", "D21", "D35", "D60", "D100"), col = circleCol[1:11], cex = 1, pch = 16, bty = "n")
+
+dev.off()
