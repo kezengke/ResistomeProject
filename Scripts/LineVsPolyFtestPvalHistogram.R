857c98000d0caab5799600d665a0a357cb2ca980
KZ <zengke940420@gmail.com>
2023-05-07 20:34:04 -0400

    updated with processed counts table and metadata.

--- Scripts/LineVsPolyFtestPvalHistogram.R
+++ Scripts/LineVsPolyFtestPvalHistogram.R
@@ -1,109 +1,32 @@
 rm(list = ls())
-library(stringr)
-library("RColorBrewer")
-library("vegan")
-library("dplyr")
 library("nlme")
-#metadata
-dukeSamples<-read.csv("Duke_samples_meta.csv", header = T, sep = ",")
-rownames(dukeSamples)<-dukeSamples$Seq_sample
-rownames(dukeSamples)<-sapply(str_split(rownames(dukeSamples), "_", n = 2), `[`, 2)
-dukeSamples$ID<-paste0("D", gsub("pre","" , sapply(str_split(dukeSamples$sample, "D", n = 3), `[`, 2), ignore.case = T)) #adding sample ID
-dukeSamples$ID<-gsub("npe", "", dukeSamples$ID, ignore.case = T)
-dukeSamples<-dukeSamples %>% mutate(bins = case_when(between(dukeSamples$Timepoint, -100, -2) ~ "PRE",
-                                                     between(dukeSamples$Timepoint, -3, 10) ~ "D7",
-                                                     between(dukeSamples$Timepoint, 11, 17) ~ "D14",
-                                                     between(dukeSamples$Timepoint, 18, 24) ~ "D21",
-                                                     between(dukeSamples$Timepoint, 25, 45) ~ "D35",
-                                                     between(dukeSamples$Timepoint, 46, 75) ~ "D60",
-                                                     between(dukeSamples$Timepoint, 76, 965) ~ "D100"))
 
-#bracken
-brackenT<-read.delim("CountsTables/duke_bracken.csv", sep = ",", header = T, row.names = 1)
-brackenT<-brackenT[, -c(1,2)] #get rid of taxonomy ID and level
-brackenT<-brackenT[, grepl("num", colnames(brackenT))] #get rid of fractions
+metaData<-read.csv("metaWithBins.csv", header = T, row.names = 1)
 
-colnames(brackenT)<-gsub(".bracken.out_num", "", colnames(brackenT)) #get rid of useless info
-SampleWithDots<-sapply(str_count(colnames(brackenT), "\\."), `[`, 1) == 2
-colnames(brackenT)[SampleWithDots]<-sub("\\.", "", colnames(brackenT)[SampleWithDots]) #get rid of random "."s in sample names
-colnames(brackenT)<-sub("\\.", "-", colnames(brackenT)) #replace . with - to match with metadata
-colnames(brackenT)<-sapply(str_split(colnames(brackenT), "_", n = 2), `[`, 2) #keeping sequencing info for matching
+#counts tables
+brackenT<-read.csv("CountsTables/brackenNormalized.csv", header = T, row.names = 1, check.names = F)
+amrT<-read.csv("CountsTables/amrNormalized.csv", header = T, row.names = 1, check.names = F)
+rgiT<-read.csv("CountsTables/rgiNormalized.csv", header = T, row.names = 1, check.names = F)
+vsearchT<-read.csv("CountsTables/vsearchNormalized.csv", header = T, row.names = 1, check.names = F)
 
-#AMR
-amrT<-read.delim("CountsTables/AMR_counts.tsv", sep = "\t", header = T, row.names = 2)
-amrT<-amrT[,-1]
-amrT<-amrT[, grepl("^D", colnames(amrT))]
-colnames(amrT)<-gsub(".amrfinder.txt", "", colnames(amrT)) #get rid of useless info
-SampleWithDots<-sapply(str_count(colnames(amrT), "\\."), `[`, 1) == 2
-colnames(amrT)[SampleWithDots]<-sub("\\.", "", colnames(amrT)[SampleWithDots]) #get rid of random "."s in sample names
-colnames(amrT)<-sub("\\.", "-", colnames(amrT)) #replace . with - to match with metadata
-colnames(amrT)<-sapply(str_split(colnames(amrT), "_", n = 2), `[`, 2)
-amrT<-amrT[, -27]
+#filtering
+brackenT<-brackenT[apply(brackenT == 0, 1, sum) <= (ncol(brackenT)*0.8), ]
+amrT<-amrT[apply(amrT == 0, 1, sum) <= (ncol(amrT)*0.8), ]
+rgiT<-rgiT[apply(rgiT == 0, 1, sum) <= (ncol(rgiT)*0.8), ]
+vsearchT<-vsearchT[apply(vsearchT == 0, 1, sum) <= (ncol(vsearchT)*0.8), ]
 
-#RGI
-rgiT<-read.delim("CountsTables/RGI_counts.tsv", sep = "\t", header = T, row.names = 2)
-rgiT<-rgiT[, -1]
-rgiT<-rgiT[, grepl("^D", colnames(rgiT))]
-colnames(rgiT)<-gsub(".rgi.txt", "", colnames(rgiT)) #get rid of useless info
-SampleWithDots<-sapply(str_count(colnames(rgiT), "\\."), `[`, 1) == 2
-colnames(rgiT)[SampleWithDots]<-sub("\\.", "", colnames(rgiT)[SampleWithDots]) #get rid of random "."s in sample names
-colnames(rgiT)<-sub("\\.", "-", colnames(rgiT)) #replace . with - to match with metadata
-colnames(rgiT)<-sapply(str_split(colnames(rgiT), "_", n = 2), `[`, 2)
-
-#vsearch
-vsearchT<-read.delim("CountsTables/vsearch_counts.tsv", sep = "\t", header = T, row.names = 2)
-vsearchT<-vsearchT[, -1] #get rid of index column
-vsearchT<-vsearchT[, grepl("^D", colnames(vsearchT))]
-colnames(vsearchT)<-gsub(".txt", "", colnames(vsearchT)) #get rid of useless info
-
-SampleWithDots<-sapply(str_count(colnames(vsearchT), "\\."), `[`, 1) == 2
-colnames(vsearchT)[SampleWithDots]<-sub("\\.", "", colnames(vsearchT)[SampleWithDots]) #get rid of random "."s in sample names
-colnames(vsearchT)<-sub("\\.", "-", colnames(vsearchT)) #replace . with - to match with metadata
-colnames(vsearchT)<-sapply(str_split(colnames(vsearchT), "_", n = 2), `[`, 2)
-
-#Normalization
-n<-colSums(brackenT)
-sumx<-sum(brackenT)
-for (i in 1:ncol(brackenT)) {
-  brackenT[,i]<-brackenT[,i]/n[i]
-}
-brackenT<-log10(brackenT*(sumx/ncol(brackenT))+1)
-
-n<-colSums(amrT)
-sumx<-sum(amrT)
-for (i in 1:ncol(amrT)) {
-  amrT[,i]<-amrT[,i]/n[i]
-}
-amrT<-log10(amrT*(sumx/ncol(amrT))+1)
-
-n<-colSums(rgiT)
-sumx<-sum(rgiT)
-for (i in 1:ncol(rgiT)) {
-  rgiT[,i]<-rgiT[,i]/n[i]
-}
-rgiT<-log10(rgiT*(sumx/ncol(rgiT))+1)
-
-n<-colSums(vsearchT)
-sumx<-sum(vsearchT)
-for (i in 1:ncol(vsearchT)) {
-  vsearchT[,i]<-vsearchT[,i]/n[i]
-}
-vsearchT<-log10(vsearchT*(sumx/ncol(vsearchT))+1)
-
-#meta for each table
-metaBracken<-dukeSamples[colnames(brackenT), ]
-metaAMR<-dukeSamples[colnames(amrT), ]
-metaRGI<-dukeSamples[colnames(rgiT), ]
-metaVsearch<-dukeSamples[colnames(vsearchT), ]
+metaBRACKEN<-metaData[colnames(brackenT), , drop = F]
+metaAMR<-metaData[colnames(amrT), , drop = F]
+metaRGI<-metaData[colnames(rgiT), , drop = F]
+metaVSEARCH<-metaData[colnames(vsearchT), , drop = F]
 
 pdf("Plots/LineVsPolyFstatsPvalHistograms.pdf", width=12, height=12)
 par(mfrow=c(2, 2))
 par(mar=c(5, 6, 4, 1)+.1)
 
-brackenT<-brackenT[apply(brackenT == 0, 1, sum) <= (ncol(brackenT)*0.8), ]
 Fpval<-vector()
 for (i in 1:nrow(brackenT)) {
-  myM<-data.frame(unlist(brackenT[i, ]), metaBracken$Timepoint, metaBracken$ID)
+  myM<-data.frame(unlist(brackenT[i, ]), metaBRACKEN$Timepoint, metaBRACKEN$ID)
   colnames(myM)<-c("counts", "timePoint", "ID")
   
   lineM<-lme(counts ~ timePoint, random = ~1 | ID, data = myM)
@@ -126,7 +49,6 @@ hist(Fpval, breaks=seq(0, 1, 0.05), xlab = "p-value", col = "tan2",
 abline(v=0.05, col=gray(.5), lty=2)
 mtext(at=0.05, side=3, text=0.05, col=gray(.5))
 
-amrT<-amrT[apply(amrT == 0, 1, sum) <= (ncol(amrT)*0.8), ]
 Fpval<-vector()
 for (i in 1:nrow(amrT)) {
   myM<-data.frame(unlist(amrT[i, ]), metaAMR$Timepoint, metaAMR$ID)
@@ -152,7 +74,6 @@ hist(Fpval, breaks=seq(0, 1, 0.05), xlab = "p-value", col = "coral3",
 abline(v=0.05, col=gray(.5), lty=2)
 mtext(at=0.05, side=3, text=0.05, col=gray(.5))
 
-rgiT<-rgiT[apply(rgiT == 0, 1, sum) <= (ncol(rgiT)*0.8), ]
 Fpval<-vector()
 for (i in 1:nrow(rgiT)) {
   myM<-data.frame(unlist(rgiT[i, ]), metaRGI$Timepoint, metaRGI$ID)
@@ -178,10 +99,9 @@ hist(Fpval, breaks=seq(0, 1, 0.05), xlab = "p-value", col = "cornflowerblue",
 abline(v=0.05, col=gray(.5), lty=2)
 mtext(at=0.05, side=3, text=0.05, col=gray(.5))
 
-vsearchT<-vsearchT[apply(vsearchT == 0, 1, sum) <= (ncol(vsearchT)*0.8), ]
 Fpval<-vector()
 for (i in 1:nrow(vsearchT)) {
-  myM<-data.frame(unlist(vsearchT[i, ]), metaVsearch$Timepoint, metaVsearch$ID)
+  myM<-data.frame(unlist(vsearchT[i, ]), metaVSEARCH$Timepoint, metaVSEARCH$ID)
   colnames(myM)<-c("counts", "timePoint", "ID")
   
   lineM<-lme(counts ~ timePoint, random = ~1 | ID, data = myM)
