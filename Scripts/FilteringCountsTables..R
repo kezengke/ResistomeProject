368cb9926371f414f7517a3bf95bad2a669fd89d
KZ <zengke940420@gmail.com>
2023-05-17 15:10:02 -0400

    script for filtering counts tables.

---
+++ Scripts/FilteringCountsTables..R
@@ -0,0 +1,20 @@
+#filtering counts tables
+rm(list = ls())
+
+#counts tables
+brackenT<-read.csv("CountsTables/brackenNormalized.csv", header = T, row.names = 1, check.names = F)
+amrT<-read.csv("CountsTables/amrNormalized.csv", header = T, row.names = 1, check.names = F)
+rgiT<-read.csv("CountsTables/rgiNormalized.csv", header = T, row.names = 1, check.names = F)
+vsearchT<-read.csv("CountsTables/vsearchNormalized.csv", header = T, row.names = 1, check.names = F)
+
+lowAbundance<-which(rowMeans(brackenT)<2)
+brackenT<-brackenT[-lowAbundance, , drop = F]
+
+amrT<-amrT[apply(amrT == 0, 1, sum) <= (ncol(amrT)*0.8), ]
+rgiT<-rgiT[apply(rgiT == 0, 1, sum) <= (ncol(rgiT)*0.8), ]
+vsearchT<-vsearchT[apply(vsearchT == 0, 1, sum) <= (ncol(vsearchT)*0.8), ]
+
+write.csv(brackenT, "CountsTables/brackenFiltered.csv")
+write.csv(amrT, "CountsTables/amrFiltered.csv")
+write.csv(rgiT, "CountsTables/rgiFiltered.csv")
+write.csv(vsearchT, "CountsTables/vsearchFiltered.csv")
