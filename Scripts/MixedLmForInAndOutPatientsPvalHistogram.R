ee7a7beb3ccd231f3cacc59c1902f942b22f1bcd
KZ <zengke940420@gmail.com>
2023-05-20 18:37:08 -0400

    updated to mixed lm for time, inout, time*inout with all three pvalues.

--- Scripts/MixedLmForInAndOutPatientsPvalHistogram.R
+++ Scripts/MixedLmForInAndOutPatientsPvalHistogram.R
@@ -15,62 +15,110 @@ metaAMR<-metaData[colnames(amrT), , drop = F]
 metaRGI<-metaData[colnames(rgiT), , drop = F]
 metaVSEARCH<-metaData[colnames(vsearchT), , drop = F]
 
-pdf("Plots/MixedLmForInAndOutPatientsPvalHistograms.pdf", width=12, height=12)
-par(mfrow=c(2, 2))
+pdf("Plots/MixedLmForInAndOutPatientsPvalHistograms.pdf", width=18, height=24)
+par(mfrow=c(4, 3))
 par(mar=c(5, 6, 4, 1)+.1)
 
-modelPvals<-vector()
+modelPvals<-c()
 for (i in 1:nrow(brackenT)) {
   myM<-data.frame(unlist(brackenT[i, ]), metaBRACKEN$Timepoint, metaBRACKEN$ptInOut, metaBRACKEN$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  Model<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
-  modelPvals[i]<-anova(Model)[2,4]
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
+  modelPvals<-rbind(modelPvals, summary(Model)$tTable[c(2:4), 5])
   
 }
-hist(modelPvals, breaks=seq(0, 1, 0.05), xlab = "p-value", col = "tan2",
-     main = "Bracken(Species) Mixed LM ANOVA P-vals", 
+hist(modelPvals[,1], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "tan2",
+     main = "Bracken(Species) Mixed LM TimePoint P-vals", 
      xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
 abline(v=0.05, col=gray(.5), lty=2)
 mtext(at=0.05, side=3, text=0.05, col=gray(.5))
 
-modelPvals<-vector()
+hist(modelPvals[,2], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "tan2",
+     main = "Bracken(Species) Mixed LM InOutPatient P-vals", 
+     xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
+abline(v=0.05, col=gray(.5), lty=2)
+mtext(at=0.05, side=3, text=0.05, col=gray(.5))
+
+hist(modelPvals[,3], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "tan2",
+     main = "Bracken(Species) Mixed LM TimePoint:InOutPatient P-vals", 
+     xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
+abline(v=0.05, col=gray(.5), lty=2)
+mtext(at=0.05, side=3, text=0.05, col=gray(.5))
+
+modelPvals<-c()
 for (i in 1:nrow(amrT)) {
   myM<-data.frame(unlist(amrT[i, ]), metaAMR$Timepoint, metaAMR$ptInOut, metaAMR$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  Model<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
-  modelPvals[i]<-anova(Model)[2,4]
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
+  modelPvals<-rbind(modelPvals, summary(Model)$tTable[c(2:4), 5])
   
 }
-hist(modelPvals, breaks=seq(0, 1, 0.05), xlab = "p-value", col = "coral3",
-     main = "AMR Mixed LM ANOVA P-vals", 
+hist(modelPvals[,1], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "coral3",
+     main = "AMR Mixed LM TimePoint P-vals", 
      xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
 abline(v=0.05, col=gray(.5), lty=2)
 mtext(at=0.05, side=3, text=0.05, col=gray(.5))
 
-modelPvals<-vector()
+hist(modelPvals[,2], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "coral3",
+     main = "AMR Mixed LM InOutPatient P-vals", 
+     xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
+abline(v=0.05, col=gray(.5), lty=2)
+mtext(at=0.05, side=3, text=0.05, col=gray(.5))
+
+hist(modelPvals[,3], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "coral3",
+     main = "AMR Mixed LM TimePoint:InOutPatient P-vals", 
+     xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
+abline(v=0.05, col=gray(.5), lty=2)
+mtext(at=0.05, side=3, text=0.05, col=gray(.5))
+
+modelPvals<-c()
 for (i in 1:nrow(rgiT)) {
   myM<-data.frame(unlist(rgiT[i, ]), metaRGI$Timepoint, metaRGI$ptInOut, metaRGI$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  Model<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
-  modelPvals[i]<-anova(Model)[2,4]
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
+  modelPvals<-rbind(modelPvals, summary(Model)$tTable[c(2:4), 5])
   
 }
-hist(modelPvals, breaks=seq(0, 1, 0.05), xlab = "p-value", col = "cornflowerblue",
-     main = "RGI Mixed LM ANOVA P-vals", 
+hist(modelPvals[,1], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "cornflowerblue",
+     main = "RGI Mixed LM TimePoint P-vals", 
      xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
 abline(v=0.05, col=gray(.5), lty=2)
 mtext(at=0.05, side=3, text=0.05, col=gray(.5))
 
-modelPvals<-vector()
+hist(modelPvals[,2], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "cornflowerblue",
+     main = "RGI Mixed LM InOutPatient P-vals", 
+     xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
+abline(v=0.05, col=gray(.5), lty=2)
+mtext(at=0.05, side=3, text=0.05, col=gray(.5))
+
+hist(modelPvals[,3], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "cornflowerblue",
+     main = "RGI Mixed LM TimePoint:InOutPatient P-vals", 
+     xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
+abline(v=0.05, col=gray(.5), lty=2)
+mtext(at=0.05, side=3, text=0.05, col=gray(.5))
+
+modelPvals<-c()
 for (i in 1:nrow(vsearchT)) {
   myM<-data.frame(unlist(vsearchT[i, ]), metaVSEARCH$Timepoint, metaVSEARCH$ptInOut, metaVSEARCH$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  Model<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
-  modelPvals[i]<-anova(Model)[2,4]
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
+  modelPvals<-rbind(modelPvals, summary(Model)$tTable[c(2:4), 5])
   
 }
-hist(modelPvals, breaks=seq(0, 1, 0.05), xlab = "p-value", col = "olivedrab4",
-     main = "vsearch Mixed LM ANOVA P-vals", 
+hist(modelPvals[,1], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "olivedrab4",
+     main = "vsearch Mixed LM TimePoint P-vals", 
+     xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
+abline(v=0.05, col=gray(.5), lty=2)
+mtext(at=0.05, side=3, text=0.05, col=gray(.5))
+
+hist(modelPvals[,2], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "olivedrab4",
+     main = "vsearch Mixed LM InOutPatient P-vals", 
+     xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
+abline(v=0.05, col=gray(.5), lty=2)
+mtext(at=0.05, side=3, text=0.05, col=gray(.5))
+
+hist(modelPvals[,3], breaks=seq(0, 1, 0.05), xlab = "p-value", col = "olivedrab4",
+     main = "vsearch Mixed LM TimePoint:InOutPatient P-vals", 
      xlim=c(0,1), cex.lab = 1.5, cex.main = 1.7, cex.axis = 1.4)
 abline(v=0.05, col=gray(.5), lty=2)
 mtext(at=0.05, side=3, text=0.05, col=gray(.5))
--- Scripts/SortedMixedLmForInOutPatientsPlots.R
+++ Scripts/SortedMixedLmForInOutPatientsPlots.R
@@ -18,25 +18,30 @@ metaVSEARCH<-metaData[colnames(vsearchT), , drop = F]
 pdf("Plots/SortedMixedLmForInOutPatients(Bracken).pdf", width=12, height=18)
 par(mfrow=c(3, 2))
 par(mar=c(5, 6, 4, 1)+.1)
-pvals<-vector()
+pvals<-c()
 for (i in 1:nrow(brackenT)) {
   myM<-data.frame(unlist(brackenT[i, ]), metaBRACKEN$Timepoint, metaBRACKEN$ptInOut, metaBRACKEN$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  lineM<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
-  pvals[i]<-anova(lineM)[2,4]
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
+  pvals<-rbind(pvals, summary(Model)$tTable[c(2:4), 5])
 }
-adjPvals<-p.adjust(pvals, method = "BH")
+pvals<-data.frame(pvals)
+pvals$adjtimePoint<-p.adjust(pvals$timePoint, method = "BH")
+pvals$adjInOut<-p.adjust(pvals$InOutOutpatient, method = "BH")
+pvals$adjtimePointInOut<-p.adjust(pvals$timePoint.InOutOutpatient, method = "BH")
 
-plot_order<-order(pvals)
+plot_order<-order(pvals$timePoint.InOutOutpatient)
 for (i in 1:nrow(brackenT)) {
   mainText<-paste0(rownames(brackenT)[plot_order[i]], 
-                   "\nANOVA P=", signif(pvals[plot_order[i]], 3), ", ", "adj. ANOVA-P=", signif(adjPvals[plot_order[i]], 3))
+                   "\nTimePoint P=", signif(pvals[plot_order[i], 1], 3), ", ", "adj. TimePoint P=", signif(pvals[plot_order[i], 4], 3),
+                   "\nInOut P=", signif(pvals[plot_order[i], 2], 3), ", ", "adj. InOut P=", signif(pvals[plot_order[i], 5], 3),
+                   "\nTimePoint:InOut P=", signif(pvals[plot_order[i], 3], 3), ", ", "adj. TimePoint:InOut P=", signif(pvals[plot_order[i], 6], 3))
   myM<-data.frame(unlist(brackenT[plot_order[i], ]), metaBRACKEN$Timepoint, metaBRACKEN$ptInOut, metaBRACKEN$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  Model<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
   plot(metaBRACKEN$Timepoint, unlist(brackenT[plot_order[i],]), 
        pch = 19, col = "tan2", 
-       main = mainText,
+       main = mainText, cex.main = 1,
        xlab = "TimePoints", ylab = "Counts(Log10)")
   lines(sort(metaBRACKEN$Timepoint), fitted(Model)[order(metaBRACKEN$Timepoint)], 
         col = "grey", type = "l")
@@ -46,24 +51,30 @@ dev.off()
 pdf("Plots/SortedMixedLmForInOutPatients(AMR).pdf", width=12, height=18)
 par(mfrow=c(3, 2))
 par(mar=c(5, 6, 4, 1)+.1)
-pvals<-vector()
+pvals<-c()
 for (i in 1:nrow(amrT)) {
   myM<-data.frame(unlist(amrT[i, ]), metaAMR$Timepoint, metaAMR$ptInOut, metaAMR$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  lineM<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
-  pvals[i]<-anova(lineM)[2,4]
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
+  pvals<-rbind(pvals, summary(Model)$tTable[c(2:4), 5])
 }
-adjPvals<-p.adjust(pvals, method = "BH")
+pvals<-data.frame(pvals)
+pvals$adjtimePoint<-p.adjust(pvals$timePoint, method = "BH")
+pvals$adjInOut<-p.adjust(pvals$InOutOutpatient, method = "BH")
+pvals$adjtimePointInOut<-p.adjust(pvals$timePoint.InOutOutpatient, method = "BH")
 
-plot_order<-order(pvals)
+plot_order<-order(pvals$timePoint.InOutOutpatient)
 for (i in 1:nrow(amrT)) {
-  mainText<-paste0(rownames(amrT)[plot_order[i]], "\nANOVA P=", signif(pvals[plot_order[i]], 3), ", ", "adj. ANOVA-P=", signif(adjPvals[plot_order[i]], 3))
+  mainText<-paste0(rownames(amrT)[plot_order[i]], 
+                   "\nTimePoint P=", signif(pvals[plot_order[i], 1], 3), ", ", "adj. TimePoint P=", signif(pvals[plot_order[i], 4], 3),
+                   "\nInOut P=", signif(pvals[plot_order[i], 2], 3), ", ", "adj. InOut P=", signif(pvals[plot_order[i], 5], 3),
+                   "\nTimePoint:InOut P=", signif(pvals[plot_order[i], 3], 3), ", ", "adj. TimePoint:InOut P=", signif(pvals[plot_order[i], 6], 3))
   myM<-data.frame(unlist(amrT[plot_order[i], ]), metaAMR$Timepoint, metaAMR$ptInOut, metaAMR$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  Model<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
   plot(metaAMR$Timepoint, unlist(amrT[plot_order[i],]), 
        pch = 19, col = "coral3", 
-       main = mainText,
+       main = mainText, cex.main = 1,
        xlab = "TimePoints", ylab = "Counts(Log10)")
   lines(sort(metaAMR$Timepoint), fitted(Model)[order(metaAMR$Timepoint)], 
         col = "grey", type = "l")
@@ -73,25 +84,30 @@ dev.off()
 pdf("Plots/SortedMixedLmForInOutPatients(RGI).pdf", width=12, height=18)
 par(mfrow=c(3, 2))
 par(mar=c(5, 6, 4, 1)+.1)
-pvals<-vector()
-Fpval<-vector()
+pvals<-c()
 for (i in 1:nrow(rgiT)) {
   myM<-data.frame(unlist(rgiT[i, ]), metaRGI$Timepoint, metaRGI$ptInOut, metaRGI$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  lineM<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
-  pvals[i]<-anova(lineM)[2,4]
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
+  pvals<-rbind(pvals, summary(Model)$tTable[c(2:4), 5])
 }
-adjPvals<-p.adjust(pvals, method = "BH")
+pvals<-data.frame(pvals)
+pvals$adjtimePoint<-p.adjust(pvals$timePoint, method = "BH")
+pvals$adjInOut<-p.adjust(pvals$InOutOutpatient, method = "BH")
+pvals$adjtimePointInOut<-p.adjust(pvals$timePoint.InOutOutpatient, method = "BH")
 
-plot_order<-order(pvals)
+plot_order<-order(pvals$timePoint.InOutOutpatient)
 for (i in 1:nrow(rgiT)) {
-  mainText<-paste0(rownames(rgiT)[plot_order[i]], "\nANOVA P=", signif(pvals[plot_order[i]], 3), ", ", "adj. ANOVA-P=", signif(adjPvals[plot_order[i]], 3))
+  mainText<-paste0(rownames(rgiT)[plot_order[i]], 
+                   "\nTimePoint P=", signif(pvals[plot_order[i], 1], 3), ", ", "adj. TimePoint P=", signif(pvals[plot_order[i], 4], 3),
+                   "\nInOut P=", signif(pvals[plot_order[i], 2], 3), ", ", "adj. InOut P=", signif(pvals[plot_order[i], 5], 3),
+                   "\nTimePoint:InOut P=", signif(pvals[plot_order[i], 3], 3), ", ", "adj. TimePoint:InOut P=", signif(pvals[plot_order[i], 6], 3))
   myM<-data.frame(unlist(rgiT[plot_order[i], ]), metaRGI$Timepoint, metaRGI$ptInOut, metaRGI$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  Model<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
   plot(metaRGI$Timepoint, unlist(rgiT[plot_order[i],]), 
        pch = 19, col = "cornflowerblue", 
-       main = mainText,
+       main = mainText, cex.main = 1,
        xlab = "TimePoints", ylab = "Counts(Log10)")
   lines(sort(metaRGI$Timepoint), fitted(Model)[order(metaRGI$Timepoint)], 
         col = "grey", type = "l")
@@ -101,24 +117,30 @@ dev.off()
 pdf("Plots/SortedMixedLmForInOutPatients(vsearch).pdf", width=12, height=18)
 par(mfrow=c(3, 2))
 par(mar=c(5, 6, 4, 1)+.1)
-pvals<-vector()
+pvals<-c()
 for (i in 1:nrow(vsearchT)) {
   myM<-data.frame(unlist(vsearchT[i, ]), metaVSEARCH$Timepoint, metaVSEARCH$ptInOut, metaVSEARCH$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  lineM<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
-  pvals[i]<-anova(lineM)[2,4]
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
+  pvals<-rbind(pvals, summary(Model)$tTable[c(2:4), 5])
 }
-adjPvals<-p.adjust(pvals, method = "BH")
+pvals<-data.frame(pvals)
+pvals$adjtimePoint<-p.adjust(pvals$timePoint, method = "BH")
+pvals$adjInOut<-p.adjust(pvals$InOutOutpatient, method = "BH")
+pvals$adjtimePointInOut<-p.adjust(pvals$timePoint.InOutOutpatient, method = "BH")
 
-plot_order<-order(pvals)
+plot_order<-order(pvals$timePoint.InOutOutpatient)
 for (i in 1:nrow(vsearchT)) {
-  mainText<-paste0(rownames(vsearchT)[plot_order[i]],  "\nANOVA P=", signif(pvals[plot_order[i]], 3), ", ", "adj. ANOVA-P=", signif(adjPvals[plot_order[i]], 3))
+  mainText<-paste0(rownames(vsearchT)[plot_order[i]], 
+                   "\nTimePoint P=", signif(pvals[plot_order[i], 1], 3), ", ", "adj. TimePoint P=", signif(pvals[plot_order[i], 4], 3),
+                   "\nInOut P=", signif(pvals[plot_order[i], 2], 3), ", ", "adj. InOut P=", signif(pvals[plot_order[i], 5], 3),
+                   "\nTimePoint:InOut P=", signif(pvals[plot_order[i], 3], 3), ", ", "adj. TimePoint:InOut P=", signif(pvals[plot_order[i], 6], 3))
   myM<-data.frame(unlist(vsearchT[plot_order[i], ]), metaVSEARCH$Timepoint, metaVSEARCH$ptInOut, metaVSEARCH$ID)
   colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  Model<-lme(counts ~ timePoint + InOut, random = ~1 | ID, data = myM)
+  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
   plot(metaVSEARCH$Timepoint, unlist(vsearchT[plot_order[i],]), 
        pch = 19, col = "olivedrab4", 
-       main = mainText,
+       main = mainText, cex.main = 1,
        xlab = "TimePoints", ylab = "Counts(Log10)")
   lines(sort(metaVSEARCH$Timepoint), fitted(Model)[order(metaVSEARCH$Timepoint)], 
         col = "grey", type = "l")
