5f13f6981a611366cec98bc79d0e94bf7e8a5f7f
KZ <zengke940420@gmail.com>
2023-08-01 02:05:45 -0400

    updated linear model line fit, and disregard cutting off between 0-300 but keeping log10.

--- Scripts/LmPlotsForMDSAndTimeXInOut.R
+++ Scripts/LmPlotsForMDSAndTimeXInOut.R
@@ -32,85 +32,43 @@ plotFun <- function(mdsNum, MDS, meta, Tname) {
                     "\nInOut(P):", signif(pvals[2], 3),
                     "\nInteraction(P):", signif( pvals[2], 3)), 
        cex.main = 1, xlab = "log10(TimePoints)", ylab = paste0("MDS", mdsNum))
-  lines(sort(meta$Timepoint), fitted(Model)[order(meta$Timepoint)], 
+  lines(log10(sort(meta$Timepoint)), fitted(Model)[order(meta$Timepoint)], 
         col = "grey", type = "l")
   legend("bottomright", c("Outpatient", "Inpatient"), col = c("cornflowerblue", "darkorange"), cex = 1, pch = 16, bty = "n")
 }
 
-plotFun2 <- function(mdsNum, MDS, meta, Tname) {
-  myM<-data.frame(MDS[, mdsNum], meta$Timepoint, meta$ptInOut, meta$ID)
-  colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
-  pvals<-summary(Model)$tTable[c(2:4), 5]
-  group_colors<-c("cornflowerblue", "darkorange")[as.numeric(as.factor(meta$ptInOut))]
-  plot(meta$Timepoint, MDS[, mdsNum], 
-       pch = 19, col = group_colors, xlim = c(min(meta$Timepoint),300), 
-       main = paste0("MDS", mdsNum, "(", Tname, ")", 
-                     "\nTimePoint(P):", signif(pvals[1], 3),
-                     "\nInOut(P):", signif(pvals[2], 3),
-                     "\nInteraction(P):", signif( pvals[2], 3)), 
-       cex.main = 1, xlab = "TimePoints", ylab = paste0("MDS", mdsNum))
-  lines(sort(meta$Timepoint), fitted(Model)[order(meta$Timepoint)], 
-        col = "grey", type = "l")
-  legend("bottomright", c("Outpatient", "Inpatient"), col = c("cornflowerblue", "darkorange"), cex = 1, pch = 16, bty = "n")
-}
-
-pdf("Plots/MDSlms.pdf", width=24, height=30)
-par(mfrow=c(5, 4))
+pdf("Plots/MDSlms.pdf", width=12, height=18)
+par(mfrow=c(3, 2))
 par(mar=c(5, 6, 4, 1)+.1)
 #Species
 MDS<-summary(capscale(t(speciesT)~1,distance = "bray"))[["sites"]][,c(1,2,3,4,5)]
 
 plotFun(1, MDS, metaSPECIES, "Species")
-plotFun2(1, MDS, metaSPECIES, "Species")
 plotFun(2, MDS, metaSPECIES, "Species")
-plotFun2(2, MDS, metaSPECIES, "Species")
-# plotFun(3, MDS, metaSPECIES, "Species")
-# plotFun(4, MDS, metaSPECIES, "Species")
-# plotFun(5, MDS, metaSPECIES, "Species")
 
 #Genus
 MDS<-summary(capscale(t(genusT)~1,distance = "bray"))[["sites"]][,c(1,2,3,4,5)]
 
 plotFun(1, MDS, metaGENUS, "Genus")
-plotFun2(1, MDS, metaGENUS, "Genus")
 plotFun(2, MDS, metaGENUS, "Genus")
-plotFun2(2, MDS, metaGENUS, "Genus")
-# plotFun(3, MDS, metaGENUS, "Genus")
-# plotFun(4, MDS, metaGENUS, "Genus")
-# plotFun(5, MDS, metaGENUS, "Genus")
 
 #AMR
 MDS<-summary(capscale(t(amrT)~1,distance = "bray"))[["sites"]][,c(1,2,3,4,5)]
 
 plotFun(1, MDS, metaAMR, "AMR")
-plotFun2(1, MDS, metaAMR, "AMR")
 plotFun(2, MDS, metaAMR, "AMR")
-plotFun2(2, MDS, metaAMR, "AMR")
-# plotFun(3, MDS, metaAMR, "AMR")
-# plotFun(4, MDS, metaAMR, "AMR")
-# plotFun(5, MDS, metaAMR, "AMR")
 
 #RGI
 MDS<-summary(capscale(t(rgiT)~1,distance = "bray"))[["sites"]][,c(1,2,3,4,5)]
 
 plotFun(1, MDS, metaRGI, "RGI")
-plotFun2(1, MDS, metaRGI, "RGI")
 plotFun(2, MDS, metaRGI, "RGI")
-plotFun2(2, MDS, metaRGI, "RGI")
-# plotFun(3, MDS, metaRGI, "RGI")
-# plotFun(4, MDS, metaRGI, "RGI")
-# plotFun(5, MDS, metaRGI, "RGI")
 
 #vsearch
 MDS<-summary(capscale(t(vsearchT)~1,distance = "bray"))[["sites"]][,c(1,2,3,4,5)]
 
 plotFun(1, MDS, metaVSEARCH, "VSEARCH")
-plotFun2(1, MDS, metaVSEARCH, "VSEARCH")
 plotFun(2, MDS, metaVSEARCH, "VSEARCH")
-plotFun2(2, MDS, metaVSEARCH, "VSEARCH")
-# plotFun(3, MDS, metaVSEARCH, "VSEARCH")
-# plotFun(4, MDS, metaVSEARCH, "VSEARCH")
-# plotFun(5, MDS, metaVSEARCH, "VSEARCH")
 
 dev.off()
+
--- Scripts/LmPlotsForShannonDiversityAndTimeXInOut.R
+++ Scripts/LmPlotsForShannonDiversityAndTimeXInOut.R
@@ -34,44 +34,19 @@ plotFun <- function(countsT, meta, Tname) {
                      "\nInOut(P):", signif(pvals[2], 3),
                      "\nInteraction(P):", signif( pvals[3], 3)), 
        cex.main = 1, xlab = "log10(TimePoints)", ylab = "ShannonDiversity")
-  lines(sort(meta$Timepoint), fitted(Model)[order(meta$Timepoint)], 
+  lines(log10(sort(meta$Timepoint)), fitted(Model)[order(meta$Timepoint)], 
         col = "grey", type = "l")
   legend("bottomright", c("Outpatient", "Inpatient"), col = c("cornflowerblue", "darkorange"), cex = 1, pch = 16, bty = "n")
 }
 
-plotFun2 <- function(countsT, meta, Tname) {
-  H3<-diversity(countsT, MARGIN = 2)
-  
-  myM<-data.frame(H3, meta$Timepoint, meta$ptInOut, meta$ID)
-  colnames(myM)<-c("counts", "timePoint", "InOut", "ID")
-  Model<-lme(counts ~ timePoint * InOut, random = ~1 | ID, data = myM)
-  pvals<-summary(Model)$tTable[c(2:4), 5]
-  group_colors<-c("cornflowerblue", "darkorange")[as.numeric(as.factor(meta$ptInOut))]
-  plot(meta$Timepoint, H3, 
-       pch = 19, col = group_colors, xlim = c(min(meta$Timepoint),300),
-       main = paste0("ShannonDiversity(", Tname, ")", 
-                     "\nTimePoint(P):", signif(pvals[1], 3),
-                     "\nInOut(P):", signif(pvals[2], 3),
-                     "\nInteraction(P):", signif( pvals[3], 3)), 
-       cex.main = 1, xlab = "TimePoints", ylab = "ShannonDiversity")
-  lines(sort(meta$Timepoint), fitted(Model)[order(meta$Timepoint)], 
-        col = "grey", type = "l")
-  legend("bottomright", c("Outpatient", "Inpatient"), col = c("cornflowerblue", "darkorange"), cex = 1, pch = 16, bty = "n")
-}
-
-pdf("Plots/ShannonDiversitylms.pdf", width=12, height=30)
-par(mfrow=c(5, 2))
+pdf("Plots/ShannonDiversitylms.pdf", width=12, height=18)
+par(mfrow=c(3, 2))
 par(mar=c(5, 6, 4, 1)+.1)
 
 plotFun(speciesT, metaSPECIES, "Species")
-plotFun2(speciesT, metaSPECIES, "Species")
 plotFun(genusT, metaGENUS, "Genus")
-plotFun2(genusT, metaGENUS, "Genus")
 plotFun(amrT, metaAMR, "AMR")
-plotFun2(amrT, metaAMR, "AMR")
 plotFun(rgiT, metaRGI, "RGI")
-plotFun2(rgiT, metaRGI, "RGI")
 plotFun(vsearchT, metaVSEARCH, "Vsearch")
-plotFun2(vsearchT, metaVSEARCH, "Vsearch")
 
 dev.off()
