714ef616e7d6e2c97e9245ea244572b989e9da56
KZ <zengke940420@gmail.com>
2023-11-11 16:01:35 -1000

    added phylum and pathway.

--- Scripts/MeanAbundanceHistogramForFiltering.R
+++ Scripts/MeanAbundanceHistogramForFiltering.R
@@ -1,12 +1,28 @@
-pdf("Plots/MeanAbundance(Log).pdf", width=28, height=7)
-par(mfrow=c(1,4))
+pdf("Plots/MeanAbundance(Log).pdf", width=21, height=14)
+par(mfrow=c(2,3))
 #Bracken
 rm(list = ls())
 myT<-read.csv("CountsTables/brackenNormalized.csv", row.name = 1, header = T, check.names = F)
 hist.data<-hist(rowMeans(myT), breaks = 50, plot=F)
 hist.data$counts<-log10(hist.data$counts+1)
 plot(hist.data, cex.lab=1.5,
-     main = "Bracken(species)", 
+     main = "Species", 
+     xlab = "taxa mean abundance", ylab = "log10(Frequency)")
+#genus
+rm(list = ls())
+myT<-read.csv("CountsTables/genusNormalized.csv", row.name = 1, header = T, check.names = F)
+hist.data<-hist(rowMeans(myT), breaks = 50, plot=F)
+hist.data$counts<-log10(hist.data$counts+1)
+plot(hist.data, cex.lab=1.5,
+     main = "Genus", 
+     xlab = "taxa mean abundance", ylab = "log10(Frequency)")
+#Phylum
+rm(list = ls())
+myT<-read.csv("CountsTables/phylumNormalized.csv", row.name = 1, header = T, check.names = F)
+hist.data<-hist(rowMeans(myT), breaks = 50, plot=F)
+hist.data$counts<-log10(hist.data$counts+1)
+plot(hist.data, cex.lab=1.5,
+     main = "Phylum", 
      xlab = "taxa mean abundance", ylab = "log10(Frequency)")
 #AMR
 rm(list = ls())
@@ -33,6 +49,15 @@ hist.data$counts<-log10(hist.data$counts+1)
 plot(hist.data, cex.lab=1.5,
      main = "vsearch", 
      xlab = "gene mean abundance", ylab = "log10(Frequency)")
+
+#PATHWAY
+rm(list = ls())
+myT<-read.csv("CountsTables/pathNormalized.csv", header = T, row.names = 1, check.names = F)
+hist.data<-hist(rowMeans(myT), breaks = 50, plot=F)
+hist.data$counts<-log10(hist.data$counts+1)
+plot(hist.data, cex.lab=1.5,
+     main = "PathAbundance", 
+     xlab = "pathway mean abundance", ylab = "log10(Frequency)")
 
 dev.off()
 
