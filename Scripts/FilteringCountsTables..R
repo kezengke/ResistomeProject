d033fc354007bb4fd547c1a3ccca9a488df39fd1
KZ <zengke940420@gmail.com>
2023-11-11 15:50:43 -1000

    added phylum and pathway.

--- Scripts/FilteringCountsTables..R
+++ Scripts/FilteringCountsTables..R
@@ -7,6 +7,8 @@ amrT<-read.csv("CountsTables/amrNormalized.csv", header = T, row.names = 1, chec
 rgiT<-read.csv("CountsTables/rgiNormalized.csv", header = T, row.names = 1, check.names = F)
 vsearchT<-read.csv("CountsTables/vsearchNormalized.csv", header = T, row.names = 1, check.names = F)
 genusT<-read.csv("CountsTables/genusNormalized.csv", header = T, row.names = 1, check.names = F)
+phylumT<-read.csv("CountsTables/phylumNormalized.csv", header = T, row.names = 1, check.names = F)
+pathT<-read.csv("CountsTables/pathNormalized.csv", header = T, row.names = 1, check.names = F)
 
 lowAbundance<-which(rowMeans(brackenT)<2)
 brackenT<-brackenT[-lowAbundance, , drop = F]
@@ -14,12 +16,20 @@ brackenT<-brackenT[-lowAbundance, , drop = F]
 lowAbundance<-which(rowMeans(genusT)<2)
 genusT<-genusT[-lowAbundance, , drop = F]
 
+lowAbundance<-which(rowMeans(phylumT)<2)
+phylumT<-phylumT[-lowAbundance, , drop = F]
+
 amrT<-amrT[apply(amrT == 0, 1, sum) <= (ncol(amrT)*0.8), ]
 rgiT<-rgiT[apply(rgiT == 0, 1, sum) <= (ncol(rgiT)*0.8), ]
 vsearchT<-vsearchT[apply(vsearchT == 0, 1, sum) <= (ncol(vsearchT)*0.8), ]
+
+lowAbundance<-which(rowMeans(pathT)<2)
+pathT<-pathT[-lowAbundance, , drop = F]
 
 write.csv(brackenT, "CountsTables/brackenFiltered.csv")
 write.csv(amrT, "CountsTables/amrFiltered.csv")
 write.csv(rgiT, "CountsTables/rgiFiltered.csv")
 write.csv(vsearchT, "CountsTables/vsearchFiltered.csv")
 write.csv(genusT, "CountsTables/genusFiltered.csv")
+write.csv(phylumT, "CountsTables/phylumFiltered.csv")
+write.csv(pathT, "CountsTables/pathFiltered.csv")
