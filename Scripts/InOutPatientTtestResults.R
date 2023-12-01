c2f9e9ef85bc35b42b1164e5e7d1a73fd12fa0b4
KZ <zengke940420@gmail.com>
2023-12-01 02:56:07 -0500

    updated wilcoxon calc and results.

--- Scripts/InOutPatientTtestResults.R
+++ Scripts/InOutPatientTtestResults.R
@@ -1,105 +1,59 @@
 #t-test for in/out patients
 rm(list = ls())
 
-metaData<-read.csv("metaWithBins.csv", header = T, row.names = 1)
+metaData<-read.csv("MetaBinnedWithInOutDonorTreatmentType.csv", header = T, row.names = 1)
 
 #counts tables
-brackenT<-read.csv("CountsTables/brackenFiltered.csv", header = T, row.names = 1, check.names = F)
+speciesT<-read.csv("CountsTables/brackenFiltered.csv", header = T, row.names = 1, check.names = F)
+genusT<-read.csv("CountsTables/genusFiltered.csv", header = T, row.names = 1, check.names = F)
+phylumT<-read.csv("CountsTables/phylumFiltered.csv", header = T, row.names = 1, check.names = F)
 amrT<-read.csv("CountsTables/amrFiltered.csv", header = T, row.names = 1, check.names = F)
 rgiT<-read.csv("CountsTables/rgiFiltered.csv", header = T, row.names = 1, check.names = F)
 vsearchT<-read.csv("CountsTables/vsearchFiltered.csv", header = T, row.names = 1, check.names = F)
-genusT<-read.csv("CountsTables/genusFiltered.csv", header = T, row.names = 1, check.names = F)
+pathwayT<-read.csv("CountsTables/pathFiltered.csv", header = T, row.names = 1, check.names = F)
 
 patients<-unique(metaData$ID)
 
-pres<-vector()
-posts<-vector()
-for (i in 1:length(patients)) {
-  patient<-metaData[metaData$ID == patients[i], , drop = F]
-  pres[i]<-rownames(patient)[patient$bins == "PRE"]
-  posts[i]<-rownames(patient)[which.min(abs(patient$Timepoint - 28))]
+pres<-rownames(metaData)[metaData$bins == "PRE"]
+posts<-rownames(metaData)[metaData$bins == "D30"]
+
+ttestFun <- function(counts, pres, posts, meta, tableType) {
+  counts<-counts[, intersect(colnames(counts), rownames(meta)), drop = F]
+  meta<-meta[intersect(colnames(counts), rownames(meta)), , drop = F]
+  
+  metaPost<-meta[posts, , drop = F]
+  
+  metaPre<-meta[pres, , drop = F]
+  metaPre<-metaPre[metaPre$ID %in% metaPost$ID, , drop = F]
+  
+  PreT<-counts[, rownames(metaPre), drop = F]
+  PostT<-counts[, rownames(metaPost), drop = F]
+  
+  #p-values
+  PreTtest_p<-apply(PreT, 1, function(x){t.test(unlist(x)~metaPre$ptInOut)$p.value})
+  PostTtest_p<-apply(PostT, 1, function(x){t.test(unlist(x)~metaPost$ptInOut)$p.value})
+
+  #adj.p-values
+  PreTtest_adj_p<-p.adjust(PreTtest_p, method = "BH")
+  PostTtest_adj_p<-p.adjust(PostTtest_p, method = "BH")
+
+  PreResults<-data.frame(PreTtest_p, PreTtest_adj_p)
+  PostResults<-data.frame(PostTtest_p, PostTtest_adj_p)
+
+  rownames(PreResults)<-rownames(counts)
+  rownames(PostResults)<-rownames(counts)
+
+  write.csv(PreResults, paste0("Ttest/", tableType, "PreInOut.csv"))
+  write.csv(PostResults, paste0("Ttest/", tableType, "PostInOut.csv"))
+
 }
 
-brackenPre<-brackenT[, pres, drop = F]
-brackenPost<-brackenT[, posts, drop = F]
-amrPre<-amrT[, pres, drop = F]
-amrPost<-amrT[, intersect(colnames(amrT), posts), drop = F]
-rgiPre<-rgiT[, pres, drop = F]
-rgiPost<-rgiT[, posts, drop = F]
-vsearchPre<-vsearchT[, pres, drop = F]
-vsearchPost<-vsearchT[, posts, drop = F]
-genusPre<-genusT[, pres, drop = F]
-genusPost<-genusT[, posts, drop = F]
+ttestFun(counts = speciesT, pres = pres, posts = posts, meta = metaData, tableType = "species")
+ttestFun(counts = genusT, pres = pres, posts = posts, meta = metaData, tableType = "genus")
+ttestFun(counts = phylumT, pres = pres, posts = posts, meta = metaData, tableType = "phylum")
 
-metaBRACKENpre<-metaData[colnames(brackenPre), , drop = F]
-metaBRACKENpost<-metaData[colnames(brackenPost), , drop = F]
-metaAMRpre<-metaData[colnames(amrPre), , drop = F]
-metaAMRpost<-metaData[colnames(amrPost), , drop = F]
-metaRGIpre<-metaData[colnames(rgiPre), , drop = F]
-metaRGIpost<-metaData[colnames(rgiPost), , drop = F]
-metaVSEARCHpre<-metaData[colnames(vsearchPre), , drop = F]
-metaVSEARCHpost<-metaData[colnames(vsearchPost), , drop = F]
-metaGENUSpre<-metaData[colnames(genusPre), , drop = F]
-metaGENUSpost<-metaData[colnames(genusPost), , drop = F]
+ttestFun(counts = amrT, pres = pres, posts = posts, meta = metaData, tableType = "AMR")
+ttestFun(counts = rgiT, pres = pres, posts = posts, meta = metaData, tableType = "RGI")
+ttestFun(counts = vsearchT, pres = pres, posts = posts, meta = metaData, tableType = "vsearch")
 
-#p-values
-brackenPreTtest_p<-apply(brackenPre, 1, function(x){t.test(unlist(x)~metaBRACKENpre$ptInOut)$p.value})
-brackenPostTtest_p<-apply(brackenPost, 1, function(x){t.test(unlist(x)~metaBRACKENpost$ptInOut)$p.value})
-amrPreTtest_p<-apply(amrPre, 1, function(x){t.test(unlist(x)~metaAMRpre$ptInOut)$p.value})
-amrPostTtest_p<-apply(amrPost, 1, function(x){t.test(unlist(x)~metaAMRpost$ptInOut)$p.value})
-rgiPreTtest_p<-apply(rgiPre, 1, function(x){t.test(unlist(x)~metaRGIpre$ptInOut)$p.value})
-rgiPostTtest_p<-apply(rgiPost, 1, function(x){t.test(unlist(x)~metaRGIpost$ptInOut)$p.value})
-vsearchPreTtest_p<-apply(vsearchPre, 1, function(x){t.test(unlist(x)~metaVSEARCHpre$ptInOut)$p.value})
-vsearchPostTtest_p<-apply(vsearchPost, 1, function(x){t.test(unlist(x)~metaVSEARCHpost$ptInOut)$p.value})
-genusPreTtest_p<-apply(genusPre, 1, function(x){t.test(unlist(x)~metaGENUSpre$ptInOut)$p.value})
-genusPostTtest_p<-apply(genusPost, 1, function(x){t.test(unlist(x)~metaGENUSpost$ptInOut)$p.value})
-
-#adj.p-values
-brackenPreTtest_adj_p<-p.adjust(brackenPreTtest_p, method = "BH")
-brackenPostTtest_adj_p<-p.adjust(brackenPostTtest_p, method = "BH")
-amrPreTtest_adj_p<-p.adjust(amrPreTtest_p, method = "BH")
-amrPostTtest_adj_p<-p.adjust(amrPostTtest_p, method = "BH")
-rgiPreTtest_adj_p<-p.adjust(rgiPreTtest_p, method = "BH")
-rgiPostTtest_adj_p<-p.adjust(rgiPostTtest_p, method = "BH")
-vsearchPreTtest_adj_p<-p.adjust(vsearchPreTtest_p, method = "BH")
-vsearchPostTtest_adj_p<-p.adjust(vsearchPostTtest_p, method = "BH")
-genusPreTtest_adj_p<-p.adjust(genusPreTtest_p, method = "BH")
-genusPostTtest_adj_p<-p.adjust(genusPostTtest_p, method = "BH")
-
-
-brackenPreResults<-data.frame(brackenPreTtest_p, brackenPreTtest_adj_p)
-brackenPostResults<-data.frame(brackenPostTtest_p, brackenPostTtest_adj_p)
-amrPreResults<-data.frame(amrPreTtest_p, amrPreTtest_adj_p)
-amrPostResults<-data.frame(amrPostTtest_p, amrPostTtest_adj_p)
-rgiPreResults<-data.frame(rgiPreTtest_p, rgiPreTtest_adj_p)
-rgiPostResults<-data.frame(rgiPostTtest_p, rgiPostTtest_adj_p)
-vsearchPreResults<-data.frame(vsearchPreTtest_p, vsearchPreTtest_adj_p)
-vsearchPostResults<-data.frame(vsearchPostTtest_p, vsearchPostTtest_adj_p)
-genusPreResults<-data.frame(genusPreTtest_p, genusPreTtest_adj_p)
-genusPostResults<-data.frame(genusPostTtest_p, genusPostTtest_adj_p)
-
-
-rownames(brackenPreResults)<-rownames(brackenT)
-rownames(brackenPostResults)<-rownames(brackenT)
-rownames(amrPreResults)<-rownames(amrT)
-rownames(amrPostResults)<-rownames(amrT)
-rownames(rgiPreResults)<-rownames(rgiT)
-rownames(rgiPostResults)<-rownames(rgiT)
-rownames(vsearchPreResults)<-rownames(vsearchT)
-rownames(vsearchPostResults)<-rownames(vsearchT)
-rownames(genusPreResults)<-rownames(genusT)
-rownames(genusPostResults)<-rownames(genusT)
-
-write.csv(brackenPreResults, "Ttest/brackenPreInOut.csv")
-write.csv(brackenPostResults, "Ttest/brackenPostInOut.csv")
-write.csv(amrPreResults, "Ttest/amrPreInOut.csv")
-write.csv(amrPostResults, "Ttest/amrPostInOut.csv")
-write.csv(rgiPreResults, "Ttest/rgiPreInOut.csv")
-write.csv(rgiPostResults, "Ttest/rgiPostInOut.csv")
-write.csv(vsearchPreResults, "Ttest/vsearchPreInOut.csv")
-write.csv(vsearchPostResults, "Ttest/vsearchPostInOut.csv")
-write.csv(genusPreResults, "Ttest/genusPreInOut.csv")
-write.csv(genusPostResults, "Ttest/genusPostInOut.csv")
-
-
-
+ttestFun(counts = pathwayT, pres = pres, posts = posts, meta = metaData, tableType = "pathway")
--- Scripts/InOutPatientWilcoxResults.R
+++ Scripts/InOutPatientWilcoxResults.R
@@ -1,105 +1,59 @@
 #wilcoxon test for in/out patients
 rm(list = ls())
 
-metaData<-read.csv("metaWithBins.csv", header = T, row.names = 1)
+metaData<-read.csv("MetaBinnedWithInOutDonorTreatmentType.csv", header = T, row.names = 1)
 
 #counts tables
-brackenT<-read.csv("CountsTables/brackenFiltered.csv", header = T, row.names = 1, check.names = F)
+speciesT<-read.csv("CountsTables/brackenFiltered.csv", header = T, row.names = 1, check.names = F)
+genusT<-read.csv("CountsTables/genusFiltered.csv", header = T, row.names = 1, check.names = F)
+phylumT<-read.csv("CountsTables/phylumFiltered.csv", header = T, row.names = 1, check.names = F)
 amrT<-read.csv("CountsTables/amrFiltered.csv", header = T, row.names = 1, check.names = F)
 rgiT<-read.csv("CountsTables/rgiFiltered.csv", header = T, row.names = 1, check.names = F)
 vsearchT<-read.csv("CountsTables/vsearchFiltered.csv", header = T, row.names = 1, check.names = F)
-genusT<-read.csv("CountsTables/genusFiltered.csv", header = T, row.names = 1, check.names = F)
+pathwayT<-read.csv("CountsTables/pathFiltered.csv", header = T, row.names = 1, check.names = F)
 
 patients<-unique(metaData$ID)
 
-pres<-vector()
-posts<-vector()
-for (i in 1:length(patients)) {
-  patient<-metaData[metaData$ID == patients[i], , drop = F]
-  pres[i]<-rownames(patient)[patient$bins == "PRE"]
-  posts[i]<-rownames(patient)[which.min(abs(patient$Timepoint - 28))]
+pres<-rownames(metaData)[metaData$bins == "PRE"]
+posts<-rownames(metaData)[metaData$bins == "D30"]
+
+wilcoxFun <- function(counts, pres, posts, meta, tableType) {
+  counts<-counts[, intersect(colnames(counts), rownames(meta)), drop = F]
+  meta<-meta[intersect(colnames(counts), rownames(meta)), , drop = F]
+  
+  metaPost<-meta[posts, , drop = F]
+  
+  metaPre<-meta[pres, , drop = F]
+  metaPre<-metaPre[metaPre$ID %in% metaPost$ID, , drop = F]
+  
+  PreT<-counts[, rownames(metaPre), drop = F]
+  PostT<-counts[, rownames(metaPost), drop = F]
+  
+  #p-values
+  PreWilcox_p<-apply(PreT, 1, function(x){wilcox.test(unlist(x)~metaPre$ptInOut)$p.value})
+  PostWilcox_p<-apply(PostT, 1, function(x){wilcox.test(unlist(x)~metaPost$ptInOut)$p.value})
+  
+  #adj.p-values
+  PreTtest_adj_p<-p.adjust(PreWilcox_p, method = "BH")
+  PostTtest_adj_p<-p.adjust(PostWilcox_p, method = "BH")
+  
+  PreResults<-data.frame(PreWilcox_p, PreTtest_adj_p)
+  PostResults<-data.frame(PostWilcox_p, PostTtest_adj_p)
+  
+  rownames(PreResults)<-rownames(counts)
+  rownames(PostResults)<-rownames(counts)
+  
+  write.csv(PreResults, paste0("Wilcox/", tableType, "PreInOut.csv"))
+  write.csv(PostResults, paste0("Wilcox/", tableType, "PostInOut.csv"))
+  
 }
 
-brackenPre<-brackenT[, pres, drop = F]
-brackenPost<-brackenT[, posts, drop = F]
-amrPre<-amrT[, pres, drop = F]
-amrPost<-amrT[, intersect(colnames(amrT), posts), drop = F]
-rgiPre<-rgiT[, pres, drop = F]
-rgiPost<-rgiT[, posts, drop = F]
-vsearchPre<-vsearchT[, pres, drop = F]
-vsearchPost<-vsearchT[, posts, drop = F]
-genusPre<-genusT[, pres, drop = F]
-genusPost<-genusT[, posts, drop = F]
+wilcoxFun(counts = speciesT, pres = pres, posts = posts, meta = metaData, tableType = "species")
+wilcoxFun(counts = genusT, pres = pres, posts = posts, meta = metaData, tableType = "genus")
+wilcoxFun(counts = phylumT, pres = pres, posts = posts, meta = metaData, tableType = "phylum")
 
-metaBRACKENpre<-metaData[colnames(brackenPre), , drop = F]
-metaBRACKENpost<-metaData[colnames(brackenPost), , drop = F]
-metaAMRpre<-metaData[colnames(amrPre), , drop = F]
-metaAMRpost<-metaData[colnames(amrPost), , drop = F]
-metaRGIpre<-metaData[colnames(rgiPre), , drop = F]
-metaRGIpost<-metaData[colnames(rgiPost), , drop = F]
-metaVSEARCHpre<-metaData[colnames(vsearchPre), , drop = F]
-metaVSEARCHpost<-metaData[colnames(vsearchPost), , drop = F]
-metaGENUSpre<-metaData[colnames(genusPre), , drop = F]
-metaGENUSpost<-metaData[colnames(genusPost), , drop = F]
+wilcoxFun(counts = amrT, pres = pres, posts = posts, meta = metaData, tableType = "AMR")
+wilcoxFun(counts = rgiT, pres = pres, posts = posts, meta = metaData, tableType = "RGI")
+wilcoxFun(counts = vsearchT, pres = pres, posts = posts, meta = metaData, tableType = "vsearch")
 
-#p-values
-brackenPreWilcox_p<-apply(brackenPre, 1, function(x){wilcox.test(unlist(x)~metaBRACKENpre$ptInOut)$p.value})
-brackenPostWilcox_p<-apply(brackenPost, 1, function(x){wilcox.test(unlist(x)~metaBRACKENpost$ptInOut)$p.value})
-amrPreWilcox_p<-apply(amrPre, 1, function(x){wilcox.test(unlist(x)~metaAMRpre$ptInOut)$p.value})
-amrPostWilcox_p<-apply(amrPost, 1, function(x){wilcox.test(unlist(x)~metaAMRpost$ptInOut)$p.value})
-rgiPreWilcox_p<-apply(rgiPre, 1, function(x){wilcox.test(unlist(x)~metaRGIpre$ptInOut)$p.value})
-rgiPostWilcox_p<-apply(rgiPost, 1, function(x){wilcox.test(unlist(x)~metaRGIpost$ptInOut)$p.value})
-vsearchPreWilcox_p<-apply(vsearchPre, 1, function(x){wilcox.test(unlist(x)~metaVSEARCHpre$ptInOut)$p.value})
-vsearchPostWilcox_p<-apply(vsearchPost, 1, function(x){wilcox.test(unlist(x)~metaVSEARCHpost$ptInOut)$p.value})
-genusPreWilcox_p<-apply(genusPre, 1, function(x){wilcox.test(unlist(x)~metaGENUSpre$ptInOut)$p.value})
-genusPostWilcox_p<-apply(genusPost, 1, function(x){wilcox.test(unlist(x)~metaGENUSpost$ptInOut)$p.value})
-
-#adj.p-values
-brackenPreWilcox_adj_p<-p.adjust(brackenPreWilcox_p, method = "BH")
-brackenPostWilcox_adj_p<-p.adjust(brackenPostWilcox_p, method = "BH")
-amrPreWilcox_adj_p<-p.adjust(amrPreWilcox_p, method = "BH")
-amrPostWilcox_adj_p<-p.adjust(amrPostWilcox_p, method = "BH")
-rgiPreWilcox_adj_p<-p.adjust(rgiPreWilcox_p, method = "BH")
-rgiPostWilcox_adj_p<-p.adjust(rgiPostWilcox_p, method = "BH")
-vsearchPreWilcox_adj_p<-p.adjust(vsearchPreWilcox_p, method = "BH")
-vsearchPostWilcox_adj_p<-p.adjust(vsearchPostWilcox_p, method = "BH")
-genusPreWilcox_adj_p<-p.adjust(genusPreWilcox_p, method = "BH")
-genusPostWilcox_adj_p<-p.adjust(genusPostWilcox_p, method = "BH")
-
-
-brackenPreResults<-data.frame(brackenPreWilcox_p, brackenPreWilcox_adj_p)
-brackenPostResults<-data.frame(brackenPostWilcox_p, brackenPostWilcox_adj_p)
-amrPreResults<-data.frame(amrPreWilcox_p, amrPreWilcox_adj_p)
-amrPostResults<-data.frame(amrPostWilcox_p, amrPostWilcox_adj_p)
-rgiPreResults<-data.frame(rgiPreWilcox_p, rgiPreWilcox_adj_p)
-rgiPostResults<-data.frame(rgiPostWilcox_p, rgiPostWilcox_adj_p)
-vsearchPreResults<-data.frame(vsearchPreWilcox_p, vsearchPreWilcox_adj_p)
-vsearchPostResults<-data.frame(vsearchPostWilcox_p, vsearchPostWilcox_adj_p)
-genusPreResults<-data.frame(genusPreWilcox_p, genusPreWilcox_adj_p)
-genusPostResults<-data.frame(genusPostWilcox_p, genusPostWilcox_adj_p)
-
-
-rownames(brackenPreResults)<-rownames(brackenT)
-rownames(brackenPostResults)<-rownames(brackenT)
-rownames(amrPreResults)<-rownames(amrT)
-rownames(amrPostResults)<-rownames(amrT)
-rownames(rgiPreResults)<-rownames(rgiT)
-rownames(rgiPostResults)<-rownames(rgiT)
-rownames(vsearchPreResults)<-rownames(vsearchT)
-rownames(vsearchPostResults)<-rownames(vsearchT)
-rownames(genusPreResults)<-rownames(genusT)
-rownames(genusPostResults)<-rownames(genusT)
-
-write.csv(brackenPreResults, "Wilcox/brackenPreInOut.csv")
-write.csv(brackenPostResults, "Wilcox/brackenPostInOut.csv")
-write.csv(amrPreResults, "Wilcox/amrPreInOut.csv")
-write.csv(amrPostResults, "Wilcox/amrPostInOut.csv")
-write.csv(rgiPreResults, "Wilcox/rgiPreInOut.csv")
-write.csv(rgiPostResults, "Wilcox/rgiPostInOut.csv")
-write.csv(vsearchPreResults, "Wilcox/vsearchPreInOut.csv")
-write.csv(vsearchPostResults, "Wilcox/vsearchPostInOut.csv")
-write.csv(genusPreResults, "Wilcox/genusPreInOut.csv")
-write.csv(genusPostResults, "Wilcox/genusPostInOut.csv")
-
-
-
+wilcoxFun(counts = pathwayT, pres = pres, posts = posts, meta = metaData, tableType = "pathway")
